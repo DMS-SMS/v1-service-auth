74891c80ecd175ba6f67b55618ed84d7abff366e
지농 <jinhong0719@naver.com>
2020-08-19 15:57:43 +0900

    [CREATE] <dataservice/service.go> Auth 데이터 접근자(DAO 객체) 메서드 인터페이스로 정의

---
+++ dataservice/service.go
@@ -0,0 +1,31 @@
+package dataservice
+
+import (
+	"auth/model"
+	"github.com/jinzhu/gorm"
+)
+
+type AuthDataAccessor interface {
+	// 계정 생성 메서드
+	CreateStudentAuth(*model.StudentAuth) (result *model.StudentAuth, err error)
+	CreateTeacherAuth(*model.TeacherAuth) (result *model.TeacherAuth, err error)
+	CreateParentAuth(*model.ParentAuth) (result *model.ParentAuth, err error)
+
+	// 사용자 정보 추가 메서드
+	CreateStudentInform(*model.StudentInform) (result *model.StudentInform, err error)
+	CreateTeacherInform(*model.TeacherInform) (result *model.TeacherInform, err error)
+	CreateParentInform(*model.ParentInform) (result *model.ParentInform, err error)
+
+	// 정보 조회 메서드
+	GetStudentInform(sid string) (*model.StudentInform, error)
+	GetTeacherInform(tid string) (*model.TeacherInform, error)
+	GetParentInform(pid string) (*model.ParentInform, error)
+
+	// 비밀번호 변경 메서드
+	ChangeStudentAuthPw(sid string, pw string) error
+	ChangeTeacherAuthPw(tid string, pw string) error
+	ChangeParentAuthPw(pid string, pw string) error
+
+	Commit() *gorm.DB
+	Rollback() *gorm.DB
+}

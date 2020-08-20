171025a6f0f7ea696f91315e86eb47dd28e7a874
지농 <jinhong0719@naver.com>
2020-08-20 12:07:29 +0900

    [ADD] <dataservice/access/mock.go> 학생, 선생님, 학부모 계정 데이터 관련 모의 메서드 선언 추가

--- dataservice/access/mock.go
+++ dataservice/access/mock.go
@@ -1,6 +1,7 @@
 package access
 
 import (
+	"auth/model"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -12,4 +13,68 @@ func NewMock(mock *mock.Mock) Mock {
 	return Mock{
 		mock: mock,
 	}
+}
+
+// 계정 생성 메서드
+func (m Mock) CreateStudentAuth(auth *model.StudentAuth) (result *model.StudentAuth, err error) {
+	args := m.mock.Called(auth)
+	return args.Get(0).(*model.StudentAuth), args.Error(1)
+}
+
+func (m Mock) CreateTeacherAuth(auth *model.TeacherAuth) (result *model.TeacherAuth, err error) {
+	args := m.mock.Called(auth)
+	return args.Get(0).(*model.TeacherAuth), args.Error(1)
+}
+
+func (m Mock) CreateParentAuth(auth *model.ParentAuth) (result *model.ParentAuth, err error) {
+	args := m.mock.Called(auth)
+	return args.Get(0).(*model.ParentAuth), args.Error(1)
+}
+
+// 계정 ID로 계정 정보 조회 메서드
+func (m Mock) GetStudentAuthWithID(id string) (*model.StudentAuth, error) {
+	args := m.mock.Called(id)
+	return args.Get(0).(*model.StudentAuth), args.Error(1)
+}
+
+func (m Mock) GetTeacherAuthWithID(id string) (*model.TeacherAuth, error) {
+	args := m.mock.Called(id)
+	return args.Get(0).(*model.TeacherAuth), args.Error(1)
+}
+
+func (m Mock) GetParentAuthWithID(id string) (*model.ParentAuth, error) {
+	args := m.mock.Called(id)
+	return args.Get(0).(*model.ParentAuth), args.Error(1)
+}
+
+// 비밀번호 변경 메서드
+func (m Mock) ChangeStudentAuthPw(sid string, pw string) error {
+	args := m.mock.Called(sid, pw)
+	return args.Error(0)
+}
+
+func (m Mock) ChangeTeacherAuthPw(tid string, pw string) error {
+	args := m.mock.Called(tid, pw)
+	return args.Error(0)
+}
+
+func (m Mock) ChangeParentAuthPw(pid string, pw string) error {
+	args := m.mock.Called(pid, pw)
+	return args.Error(0)
+}
+
+// 계성 삭제 메서드 (Soft Delete)
+func (m Mock) DeleteStudentAuth(sid uint) error {
+	args := m.mock.Called(sid)
+	return args.Error(0)
+}
+
+func (m Mock) DeleteTeacherAuth(tid string) error {
+	args := m.mock.Called(tid)
+	return args.Error(0)
+}
+
+func (m Mock) DeleteParentAuth(pid string) error {
+	args := m.mock.Called(pid)
+	return args.Error(0)
 }

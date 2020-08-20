da9a3c597771f55dff0bfeb84652edef3ba9153f
지농 <jinhong0719@naver.com>
2020-08-20 13:55:52 +0900

    [ADD] <dataservice/access/mock.go> 사용자(학생, 선생님, 학부모) 정보 데이터 관련 모의 메서드 추가

--- dataservice/access/mock.go
+++ dataservice/access/mock.go
@@ -2,6 +2,7 @@ package access
 
 import (
 	"auth/model"
+	"github.com/jinzhu/gorm"
 	"github.com/stretchr/testify/mock"
 )
 
@@ -77,4 +78,84 @@ func (m Mock) DeleteTeacherAuth(tid string) error {
 func (m Mock) DeleteParentAuth(pid string) error {
 	args := m.mock.Called(pid)
 	return args.Error(0)
+}
+
+
+// 사용자 정보 추가 메서드
+func (m Mock) CreateStudentInform(inform *model.StudentInform) (result *model.StudentInform, err error) {
+	args := m.mock.Called(inform)
+	return args.Get(0).(*model.StudentInform), args.Error(1)
+}
+
+func (m Mock) CreateTeacherInform(inform *model.TeacherInform) (result *model.TeacherInform, err error) {
+	args := m.mock.Called(inform)
+	return args.Get(0).(*model.TeacherInform), args.Error(1)
+}
+
+func (m Mock) CreateParentInform(inform *model.ParentInform) (result *model.ParentInform, err error) {
+	args := m.mock.Called(inform)
+	return args.Get(0).(*model.ParentInform), args.Error(1)
+}
+
+// 사용자 정보로 uuid 조회 메서드 (계정 삭제 시 사용)
+func (m Mock) GetStudentUUIDWithInform(inform *model.StudentInform) (sid []string, err error) {
+	args := m.mock.Called(inform)
+	return args.Get(0).([]string), args.Error(1)
+}
+
+func (m Mock) GetTeacherUUIDWithInform(inform *model.TeacherInform) (tid []string, err error) {
+	args := m.mock.Called(inform)
+	return args.Get(0).([]string), args.Error(1)
+}
+
+func (m Mock) GetParentUUIDWithInform(inform *model.ParentInform) (pid []string, err error) {
+	args := m.mock.Called(inform)
+	return args.Get(0).([]string), args.Error(1)
+}
+
+// 정보 조회 메서드
+func (m Mock) GetStudentInformWithUUID(sid string) (*model.StudentInform, error) {
+	args := m.mock.Called(sid)
+	return args.Get(0).(*model.StudentInform), args.Error(1)
+}
+
+func (m Mock) GetTeacherInformWithUUID(tid string) (*model.TeacherInform, error) {
+	args := m.mock.Called(tid)
+	return args.Get(0).(*model.TeacherInform), args.Error(1)
+}
+
+func (m Mock) GetParentInformWithUUID(pid string) (*model.ParentInform, error) {
+	args := m.mock.Called(pid)
+	return args.Get(0).(*model.ParentInform), args.Error(1)
+}
+
+// 사용자 정보 수정 메서드
+func (m Mock) ModifyStudentInform(sid string, modify *model.StudentInform) (result *model.StudentInform, err error) {
+	args := m.mock.Called(sid, modify)
+	return args.Get(0).(*model.StudentInform), args.Error(1)
+}
+
+func (m Mock) ModifyTeacherInform(tid string, modify *model.TeacherInform) (result *model.TeacherInform, err error) {
+	args := m.mock.Called(tid, modify)
+	return args.Get(0).(*model.TeacherInform), args.Error(1)
+}
+
+func (m Mock) ModifyParentInform(pid string, modify *model.ParentInform) (result *model.ParentInform, err error) {
+	args := m.mock.Called(pid, modify)
+	return args.Get(0).(*model.ParentInform), args.Error(1)
+}
+
+// ---
+
+// 트랜잭션 관련 메서드
+func (m Mock) Begin(db *gorm.DB) {
+	m.mock.Called(db)
+}
+
+func (m Mock) Commit() *gorm.DB {
+	return m.mock.Called().Get(0).(*gorm.DB)
+}
+
+func (m Mock) Rollback() *gorm.DB {
+	return m.mock.Called().Get(0).(*gorm.DB)
 }

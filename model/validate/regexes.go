33771191467d30419cace258f7284cf80ece6215
지농 <jinhong0719@naver.com>
2020-08-21 11:32:07 +0900

    [CREATE] <model/validate/regexes.go> 문자열 형식을 단정 짓기 위한 정규식 관련 파일 생성

---
+++ model/validate/regexes.go
@@ -0,0 +1,15 @@
+package validate
+
+import "regexp"
+
+const (
+	studentUUIDRegexString = "^student-\\d\\d\\d\\d\\d\\d\\d\\d\\d\\d\\d\\d"
+	teacherUUIDRegexString = "^teacher-\\d\\d\\d\\d\\d\\d\\d\\d\\d\\d\\d\\d"
+	parentUUIDRegexString = "^parent-\\d\\d\\d\\d\\d\\d\\d\\d\\d\\d\\d\\d"
+)
+
+var (
+	studentUUIDRegex = regexp.MustCompile(studentUUIDRegexString)
+	teacherUUIDRegex = regexp.MustCompile(teacherUUIDRegexString)
+	parentUUIDRegex = regexp.MustCompile(parentUUIDRegexString)
+)

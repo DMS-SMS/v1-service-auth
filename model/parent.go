8e4d959e9bce8d9bcaeb02b526dc77fa68a12ea3
지농 <jinhong0719@naver.com>
2020-08-21 09:26:38 +0900

    [ADD] <model/(student, parent, teacher).go> 일부 pk(UUID) 및 fk 제약조건 추가

--- model/parent.go
+++ model/parent.go
@@ -4,14 +4,14 @@ import "github.com/jinzhu/gorm"
 
 type ParentAuth struct {
 	gorm.Model
-	UUID     string `gorm:"PRIMARY_KEY;Type:char(19);INDEX"` // 형식 => 'parent-' + 12자리 랜덤 수 (19자)
+	UUID     string `gorm:"PRIMARY_KEY;Type:char(19);UNIQUE;INDEX"` // 형식 => 'parent-' + 12자리 랜덤 수 (19자)
 	ParentId string `gorm:"Type:varchar(20);NOT NULL;UNIQUE"`  // 4~20자 사이
 	ParentPw string `gorm:"Type:varchar(100);NOT NULL"`
 }
 
 type ParentInform struct {
 	gorm.Model
-	ParentUUID  string `gorm:"Type:char(19);NOT NULL"` // 형식 => 'parent-' + 12자리 랜덤 수
+	ParentUUID  string `gorm:"Type:char(19);UNIQUE;NOT NULL"` // 형식 => 'parent-' + 12자리 랜덤 수
 	Name        string `gorm:"Type:char(4);NOT NULL"`  // 2~4자 사이
-	PhoneNumber string `gorm:"Type:char(11);NOT NULL"` // 11자
+	PhoneNumber string `gorm:"Type:char(11);UNIQUE;NOT NULL"` // 11자
 }

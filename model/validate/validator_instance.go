2e09336d8d6158dbf811926d1ff3536ebd0812c2
지농 <jinhong0719@naver.com>
2020-08-21 11:36:08 +0900

    [ADD] <model/validate/validator_instance.go> uuid 유효성 검사 함수 선언 및 등록

--- model/validate/validator_instance.go
+++ model/validate/validator_instance.go
@@ -2,10 +2,25 @@ package validate
 
 import (
 	"github.com/go-playground/validator/v10"
+	"log"
 )
 
 var DBValidator *validator.Validate
 
 func init() {
 	DBValidator = validator.New()
+
+	if err := DBValidator.RegisterValidation("uuid", uuidValidateFunc); err != nil { log.Fatal(err) }
+}
+
+func uuidValidateFunc(fl validator.FieldLevel) bool {
+	switch fl.Param() {
+	case "student":
+		return studentUUIDRegex.MatchString(fl.Field().String())
+	case "teacher":
+		return teacherUUIDRegex.MatchString(fl.Field().String())
+	case "parent":
+		return parentUUIDRegex.MatchString(fl.Field().String())
+	}
+	return false
 }

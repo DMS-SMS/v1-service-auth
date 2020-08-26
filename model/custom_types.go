8b6675468f3bc47ec8153756d4a6623d6d79781f
지농 <jinhong0719@naver.com>
2020-08-26 17:58:55 +0900

    [ADD] <model/custom_types.go> class 속성에 매핑되는 구조체 필드의 타입 선언 & 생성자 추가

--- model/custom_types.go
+++ model/custom_types.go
@@ -1,7 +1,6 @@
 package model
 
 import (
-	"auth/tool/arraylist"
 	"database/sql/driver"
 	"errors"
 	"fmt"
@@ -11,8 +10,22 @@ type grade struct {
 	value int64
 }
 
-var gradeAvailableValues = []int64{1, 2, 3}
-var GradeValuesList = arraylist.NewWithInt64(gradeAvailableValues...)
+type class struct {
+	value int64
+}
+
+// ---
+
+func Grade(v int64) grade {
+	return grade{v}
+}
+
+func Class(v int64) class {
+	return class{v}
+}
+
+// ---
+
 func (g grade) Value() (driver.Value, error) {
 	if !GradeValuesList.Contains(g.value) {
 		return nil, errors.New(fmt.Sprintf("%d is outside the range of the grade property", g.value))
@@ -22,5 +35,17 @@ func (g grade) Value() (driver.Value, error) {
 
 func (g *grade) Scan(v interface{}) error {
 	g.value = v.(int64)
+	return nil
+}
+
+func (c class) Value() (driver.Value, error) {
+	if !ClassValuesList.Contains(c.value) {
+		return nil, errors.New(fmt.Sprintf("%d is outside the range of the class property", c.value))
+	}
+	return c.value, nil
+}
+
+func (c *class) Scan(v interface{}) error {
+	c.value = v.(int64)
 	return nil
 }

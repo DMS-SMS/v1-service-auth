a78c51f97aac120c89cabbd82ea1427c0aa2872c
지농 <jinhong0719@naver.com>
2020-08-26 15:10:23 +0900

    [ADD] <model/custom_types.go> 특정 속성에 들어갈 수 있는 값들을 모두 모와둔 배열 선언 추가

--- model/custom_types.go
+++ model/custom_types.go
@@ -1,14 +1,22 @@
 package model
 
 import (
+	"auth/tool/arraylist"
 	"database/sql/driver"
+	"errors"
+	"fmt"
 )
 
 type grade struct {
 	value int64
 }
 
+var gradeAvailableValues = []int64{1, 2, 3}
+var GradeValuesList = arraylist.NewWithInt64(gradeAvailableValues...)
 func (g grade) Value() (driver.Value, error) {
+	if !GradeValuesList.Contains(g.value) {
+		return nil, errors.New(fmt.Sprintf("%d is outside the range of the grade property", g.value))
+	}
 	return g.value, nil
 }
 

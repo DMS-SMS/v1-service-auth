a006e542f975a565b8e3bc666c87cb40f8f8c055
지농 <jinhong0719@naver.com>
2020-08-28 12:11:54 +0900

    [ADD] <db/default_create.go> 학생, 선생님, 학부모 계정 생성 관련 메서드 선언 추가

--- db/access/default_create.go
+++ db/access/default_create.go
@@ -1,1 +1,38 @@
 package access
+
+import (
+	"auth/model"
+)
+
+func (d *Default) CreateStudentAuth(auth *model.StudentAuth) (*model.StudentAuth, error) {
+	result := d.tx.Create(auth)
+	if auth, ok := result.Value.(*model.StudentAuth); ok {
+		return auth, result.Error
+	}
+	if result.Error == nil {
+		result.Error = StudentAuthAssertionError
+	}
+	return nil, result.Error
+}
+
+func (d *Default) CreateTeacherAuth(auth *model.TeacherAuth) (*model.TeacherAuth, error) {
+	result := d.tx.Create(auth)
+	if auth, ok := result.Value.(*model.TeacherAuth); ok {
+		return auth, result.Error
+	}
+	if result.Error == nil {
+		result.Error = TeacherAuthAssertionError
+	}
+	return nil, result.Error
+}
+
+func (d *Default) CreateParentAuth(auth *model.ParentAuth) (*model.ParentAuth, error) {
+	result := d.tx.Create(auth)
+	if auth, ok := result.Value.(*model.ParentAuth); ok {
+		return auth, result.Error
+	}
+	if result.Error == nil {
+		result.Error = ParentAuthAssertionError
+	}
+	return nil, result.Error
+}

f0902d6fa0724b8a498411819366cdc70eecac8e
지농 <jinhong0719@naver.com>
2020-08-31 20:51:54 +0900

    [MODIFY] <db/accessor_manage.go> accessor 필드 타입 수정 및 그에 따른 관련된 로직 수정

--- db/accessor_manage.go
+++ db/accessor_manage.go
@@ -1,25 +1,42 @@
 package db
 
 import (
+	"errors"
+	"fmt"
 	"github.com/jinzhu/gorm"
 	"reflect"
 )
 
 type AccessorManage struct {
-	accessor Accessor
-	db *gorm.DB
+	accessorType reflect.Type
+	dbForTx *gorm.DB
 }
 
-func NewAccessorManage(accessor Accessor, db *gorm.DB) AccessorManage {
-	return AccessorManage{
-		accessor: accessor,
-		db:       db,
+func NewAccessorManage(accessorType reflect.Type, dbForTx *gorm.DB) (manager AccessorManage, err error) {
+	if accessorType == nil || dbForTx == nil {
+		err = errors.New(fmt.Sprintf("nil parameter is not allowed"))
+		return
 	}
+
+	if _, ok := reflect.New(accessorType).Elem().Interface().(Accessor); !ok {
+		err = errors.New(fmt.Sprintf("type %s is not an implement of db.Accessor", accessorType.String()))
+		return
+	}
+
+	manager = AccessorManage{
+		accessorType: accessorType,
+		dbForTx:      dbForTx,
+	}
+	return
 }
 
-func (atm AccessorManage) BeginTx() (accessor Accessor) {
-	t := reflect.TypeOf(atm.accessor)
-	accessor = reflect.New(t).Interface().(Accessor)
-	accessor.Begin(atm.db)
+func (atm AccessorManage) BeginTx() (accessor Accessor, err error) {
+	if atm.accessorType == nil || atm.dbForTx == nil {
+		err = errors.New("please create db.AccessorManage instance object through the constructor")
+		return
+	}
+
+	accessor = reflect.New(atm.accessorType.Elem()).Interface().(Accessor)
+	accessor.Begin(atm.dbForTx)
 	return
 }

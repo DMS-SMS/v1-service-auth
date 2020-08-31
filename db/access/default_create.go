983949d7d6de0985aa83417fab71c781e134d34e
지농 <jinhong0719@naver.com>
2020-08-31 22:32:10 +0900

    [MODIFY] <db/access/default_create.go> default 객체 접근 제한자 변경에 따른 리시버 변수 수정

--- db/access/default_create.go
+++ db/access/default_create.go
@@ -5,7 +5,7 @@ import (
 	"auth/model"
 )
 
-func (d *Default) CreateStudentAuth(auth *model.StudentAuth) (*model.StudentAuth, error) {
+func (d *_default) CreateStudentAuth(auth *model.StudentAuth) (*model.StudentAuth, error) {
 	result := d.tx.Create(auth)
 	if auth, ok := result.Value.(*model.StudentAuth); ok {
 		return auth, result.Error
@@ -16,7 +16,7 @@ func (d *Default) CreateStudentAuth(auth *model.StudentAuth) (*model.StudentAuth
 	return nil, result.Error
 }
 
-func (d *Default) CreateTeacherAuth(auth *model.TeacherAuth) (*model.TeacherAuth, error) {
+func (d *_default) CreateTeacherAuth(auth *model.TeacherAuth) (*model.TeacherAuth, error) {
 	result := d.tx.Create(auth)
 	if auth, ok := result.Value.(*model.TeacherAuth); ok {
 		return auth, result.Error
@@ -27,7 +27,7 @@ func (d *Default) CreateTeacherAuth(auth *model.TeacherAuth) (*model.TeacherAuth
 	return nil, result.Error
 }
 
-func (d *Default) CreateParentAuth(auth *model.ParentAuth) (*model.ParentAuth, error) {
+func (d *_default) CreateParentAuth(auth *model.ParentAuth) (*model.ParentAuth, error) {
 	result := d.tx.Create(auth)
 	if auth, ok := result.Value.(*model.ParentAuth); ok {
 		return auth, result.Error
@@ -38,7 +38,7 @@ func (d *Default) CreateParentAuth(auth *model.ParentAuth) (*model.ParentAuth, e
 	return nil, result.Error
 }
 
-func (d *Default) CreateStudentInform(inform *model.StudentInform) (*model.StudentInform, error) {
+func (d *_default) CreateStudentInform(inform *model.StudentInform) (*model.StudentInform, error) {
 	result := d.tx.Create(inform)
 	if inform, ok := result.Value.(*model.StudentInform); ok {
 		return inform, result.Error
@@ -49,7 +49,7 @@ func (d *Default) CreateStudentInform(inform *model.StudentInform) (*model.Stude
 	return nil, result.Error
 }
 
-func (d *Default) CreateTeacherInform(inform *model.TeacherInform) (*model.TeacherInform, error) {
+func (d *_default) CreateTeacherInform(inform *model.TeacherInform) (*model.TeacherInform, error) {
 	result := d.tx.Create(inform)
 	if inform, ok := result.Value.(*model.TeacherInform); ok {
 		return inform, result.Error
@@ -60,7 +60,7 @@ func (d *Default) CreateTeacherInform(inform *model.TeacherInform) (*model.Teach
 	return nil, result.Error
 }
 
-func (d *Default) CreateParentInform(inform *model.ParentInform) (*model.ParentInform, error) {
+func (d *_default) CreateParentInform(inform *model.ParentInform) (*model.ParentInform, error) {
 	result := d.tx.Create(inform)
 	if inform, ok := result.Value.(*model.ParentInform); ok {
 		return inform, result.Error

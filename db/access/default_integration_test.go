46c8fb0f337a93deee0f90ff0accf25acdbb2975
지농 <jinhong0719@naver.com>
2020-09-01 23:06:47 +0900

    [ADD] <db/access/default_integration_test.go> 테스트를 위한 기본 환경 설정 함수 및 변수 추가

--- db/access/default_integration_test.go
+++ db/access/default_integration_test.go
@@ -1,1 +1,36 @@
 package access
+
+import (
+	"auth/adapter"
+	"auth/db"
+	"github.com/hashicorp/consul/api"
+	"log"
+)
+
+var manager db.AccessorManage
+
+func init() {
+	cli, err := api.NewClient(api.DefaultConfig())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	dbc, _, err := adapter.ConnectDBWithConsul(cli, "db/auth/local_test")
+	if err != nil {
+		log.Fatal(err)
+	}
+	db.Migrate(dbc)
+	dbc.LogMode(true)
+
+	manager, err = db.NewAccessorManage(DefaultReflectType(), dbc)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+var passwords = map[string]string{
+	"testPW1": "$2a$10$POwSnghOjkriuQ4w1Bj3zeHIGA7fXv8UI/UFXEhnnO5YrcwkUDcXq",
+	"testPW2": "$2a$10$XxGXTboHZxhoqzKcBVqkJOiNSy6narAvIQ/ljfTJ4m93jAt8GyX.e",
+	"testPW3": "$2a$10$sfZLOR8iVyhXI0y8nXcKIuKseahKu4NLSlocUWqoBdGrpLIZzxJ2S",
+}
+

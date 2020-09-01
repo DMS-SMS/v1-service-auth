055de5790e8e25f04859b3c90a765009eab71c19
지농 <jinhong0719@naver.com>
2020-09-01 17:19:05 +0900

    [ADD] <model/method_for_test.go> 계정 관련 model 객체 ExceptGormModel 메서드 선언

--- model/method_for_test.go
+++ model/method_for_test.go
@@ -1,1 +1,26 @@
 package model
+
+func (sa *StudentAuth) ExceptGormModel() *StudentAuth  {
+	return &StudentAuth{
+		UUID:       sa.UUID,
+		StudentId:  sa.StudentId,
+		StudentPw:  sa.StudentPw,
+		ParentUUID: sa.ParentUUID,
+	}
+}
+
+func (ta *TeacherAuth) ExceptGormModel() *TeacherAuth  {
+	return &TeacherAuth{
+		UUID:      ta.UUID,
+		TeacherId: ta.TeacherId,
+		TeacherPw: ta.TeacherPw,
+	}
+}
+
+func (pa *ParentAuth) ExceptGormModel() *ParentAuth  {
+	return &ParentAuth{
+		UUID:     pa.UUID,
+		ParentId: pa.ParentId,
+		ParentPw: pa.ParentPw,
+	}
+}

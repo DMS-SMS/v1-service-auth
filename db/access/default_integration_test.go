bfb3e624adcc971b07521e22b0e35350c92cb0dd
지농 <jinhong0719@naver.com>
2020-09-02 08:56:58 +0900

    [ADD] <db/access/default_integration_test.go> 테스트 함수 기본적인 구조 작성

--- db/access/default_integration_test.go
+++ db/access/default_integration_test.go
@@ -3,8 +3,12 @@ package access
 import (
 	"auth/adapter"
 	"auth/db"
+	"auth/model"
+	"fmt"
 	"github.com/hashicorp/consul/api"
+	"github.com/stretchr/testify/assert"
 	"log"
+	"testing"
 )
 
 var manager db.AccessorManage
@@ -34,3 +38,62 @@ var passwords = map[string]string{
 	"testPW3": "$2a$10$sfZLOR8iVyhXI0y8nXcKIuKseahKu4NLSlocUWqoBdGrpLIZzxJ2S",
 }
 
+func TestDefault_CreateStudentAuth(t *testing.T) {
+	// Tx 시작
+	access, err := manager.BeginTx()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// StudentAuth.ParentUUID에 설정할 값을 위한 학부모 계정 생성
+	inits := []struct {
+		UUID, ParentID, ParentPW string
+	} {
+		{
+			UUID:     "parent-432143214321",
+			ParentID: "jinhong07191",
+			ParentPW: passwords["testPW1"],
+		}, {
+			UUID:     "parent-123412341234",
+			ParentID: "jinhong07192",
+			ParentPW: passwords["testPW2"],
+		},
+	}
+
+	for _, init := range inits {
+		auth := &model.ParentAuth{UUID: init.UUID, ParentId: init.ParentID, ParentPw: init.ParentPW}
+		if _, err := access.CreateParentAuth(auth); err != nil {
+			access.Rollback()
+			log.Fatal(fmt.Sprintf("error occurs while creating parent auth, err: %v", err))
+		}
+	}
+
+	tests := []struct {
+		UUID, ParentUUID, StudentID, StudentPW string
+		ExpectAuth *model.StudentAuth
+		ExpectError error
+	} {
+		{ // 정상적인 input test case
+			UUID: "student-123412341234",
+			ParentUUID: "parent-123412341234",
+			StudentID: "jinhong07191",
+			StudentPW: passwords["testPW1"],
+			ExpectError: nil,
+		},
+	}
+
+	for _, test := range tests {
+		paramAuth := &model.StudentAuth{
+			UUID:       test.UUID,
+			StudentId:  test.StudentID,
+			StudentPw:  test.StudentPW,
+			ParentUUID: test.ParentUUID,
+		}
+
+		createdAuth, err := access.CreateStudentAuth(paramAuth)
+		assert.Equalf(t, test.ExpectError, err, "error assertion error (test case: %v)", test)
+		assert.Equalf(t, paramAuth, createdAuth, "result model assertion (test case: %v)", test)
+	}
+
+	access.Rollback()
+}

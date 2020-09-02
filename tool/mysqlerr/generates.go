151c8ce269eb9ea86f9694a2b48fa30f3d890bf9
지농 <jinhong0719@naver.com>
2020-09-02 21:43:51 +0900

    [ADD] <tool/mysqlerr/genereates.go> FK 제약조건 실패 에러 생성 및 반환 함수 추가

--- tool/mysqlerr/generates.go
+++ tool/mysqlerr/generates.go
@@ -12,3 +12,17 @@ func DuplicateEntry(key, entry string) *mysql.MySQLError {
 		Message: fmt.Sprintf("Duplicate entry '%s' for key '%s'", entry, key),
 	}
 }
+
+type Reference struct {
+	TableName, AttrName string
+}
+
+func FKConstraintFail(dbName, tableName, constraintName, attrName string, ref Reference) *mysql.MySQLError {
+	prefix := "Cannot add or update a child row: a foreign key constraint fails"
+	suffix := fmt.Sprintf("(`%s`.`%s`, CONSTRAINT `%s` FOREIGN KEY (`%s`) REFERENCES `%s` (`%s`) ON DELETE RESTRICT ON UPDATE)",
+		dbName, tableName, constraintName, attrName, ref.TableName, ref.AttrName)
+	return &mysql.MySQLError{
+		Number:  mysqlerr.ER_NO_REFERENCED_ROW_2,
+		Message: prefix + " " + suffix,
+	}
+}

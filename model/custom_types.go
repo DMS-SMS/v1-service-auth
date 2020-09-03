7e6fd393cf036c0df55fa7337b7a5988b283f042
지농 <jinhong0719@naver.com>
2020-09-03 22:19:37 +0900

    [MODIFY] <model/custom_types.go> 모든 사용자 지정 타입들의 접근 제한자 public으로 변경

--- model/custom_types.go
+++ model/custom_types.go
@@ -4,65 +4,65 @@ import (
 	"database/sql/driver"
 )
 
-type grade int64
-func (g grade) Value() (value driver.Value, err error) {
+type Grade int64
+func (g Grade) Value() (value driver.Value, err error) {
 	value = int64(g)
 	if value == 0 { value = nil }
 	return
 }
-func (g *grade) Scan(v interface{}) (_ error) { *g = grade(v.(int64)); return }
-func (g grade) KeyName() string { return "grade" }
+func (g *Grade) Scan(src interface{}) (_ error) { *g = Grade(src.(int64)); return }
+func (g Grade) KeyName() string { return "grade" }
 
-type class int64
-func (c class) Value() (value driver.Value, err error) {
+type Class int64
+func (c Class) Value() (value driver.Value, err error) {
 	value = int64(c)
 	if value == 0 { value = nil }
 	return
 }
-func (c *class) Scan(v interface{}) (err error) { *c = class(v.(int64)); return }
-func (c class) KeyName() string { return "class" }
+func (c *Class) Scan(src interface{}) (err error) { *c = Class(src.(int64)); return }
+func (c Class) KeyName() string { return "class" }
 
 
-type studentNumber int64
-func (sn studentNumber) Value() (value driver.Value, err error) {
+type StudentNumber int64
+func (sn StudentNumber) Value() (value driver.Value, err error) {
 	value = int64(sn)
 	if value == 0 { value = nil }
 	return
 }
-func (sn *studentNumber) Scan(v interface{}) (err error) { *sn = studentNumber(v.(int64)); return }
-func (sn studentNumber) KeyName() string { return "student_number" }
+func (sn *StudentNumber) Scan(src interface{}) (err error) { *sn = StudentNumber(src.(int64)); return }
+func (sn StudentNumber) KeyName() string { return "student_number" }
 
-type uuid string
-func (u uuid) Value() (driver.Value, error) { return string(u), nil }
-func (u *uuid) Scan(v interface{}) (err error) { *u = uuid(v.(string)); return }
-func (u uuid) KeyName() string { return "uuid" }
+type UUID string
+func (u UUID) Value() (driver.Value, error) { return string(u), nil }
+func (u *UUID) Scan(src interface{}) (err error) { *u = UUID(src.(string)); return }
+func (u UUID) KeyName() string { return "UUID" }
 
-type studentID string
-func (si studentID) Value() (driver.Value, error) { return string(si), nil }
-func (si *studentID) Scan(v interface{}) (err error) { *si = studentID(v.(string)); return }
-func (si studentID) KeyName() string { return "student_id" }
+type StudentID string
+func (si StudentID) Value() (driver.Value, error) { return string(si), nil }
+func (si *StudentID) Scan(src interface{}) (err error) { *si = StudentID(src.(string)); return }
+func (si StudentID) KeyName() string { return "student_id" }
 
-type studentPW string
-func (sp studentPW) Value() (driver.Value, error) { return string(sp), nil }
-func (sp *studentPW) Scan(v interface{}) (err error) { *sp = studentPW(v.(string)); return }
-func (sp studentPW) KeyName() string { return "student_pw" }
+type StudentPW string
+func (sp StudentPW) Value() (driver.Value, error) { return string(sp), nil }
+func (sp *StudentPW) Scan(src interface{}) (err error) { *sp = StudentPW(src.(string)); return }
+func (sp StudentPW) KeyName() string { return "student_pw" }
 
-type teacherID string
-func (ti teacherID) Value() (driver.Value, error) { return string(ti), nil }
-func (ti *teacherID) Scan(v interface{}) (err error) { *ti = teacherID(v.(string)); return }
-func (ti teacherID) KeyName() string { return "teacher_id" }
+type TeacherID string
+func (ti TeacherID) Value() (driver.Value, error) { return string(ti), nil }
+func (ti *TeacherID) Scan(src interface{}) (err error) { *ti = TeacherID(src.(string)); return }
+func (ti TeacherID) KeyName() string { return "teacher_id" }
 
-type teacherPW string
-func (tp teacherPW) Value() (driver.Value, error) { return string(tp), nil }
-func (tp *teacherPW) Scan(v interface{}) (err error) { *tp = teacherPW(v.(string)); return }
-func (tp teacherPW) KeyName() string { return "teacher_pw" }
+type TeacherPW string
+func (tp TeacherPW) Value() (driver.Value, error) { return string(tp), nil }
+func (tp *TeacherPW) Scan(src interface{}) (err error) { *tp = TeacherPW(src.(string)); return }
+func (tp TeacherPW) KeyName() string { return "teacher_pw" }
 
-type parentID string
-func (pi parentID) Value() (driver.Value, error) { return string(pi), nil }
-func (pi *parentID) Scan(v interface{}) (err error) { *pi = parentID(v.(string)); return }
-func (pi parentID) KeyName() string { return "parent_id" }
+type ParentID string
+func (pi ParentID) Value() (driver.Value, error) { return string(pi), nil }
+func (pi *ParentID) Scan(src interface{}) (err error) { *pi = ParentID(src.(string)); return }
+func (pi ParentID) KeyName() string { return "parent_id" }
 
-type parentPW string
-func (pp parentPW) Value() (driver.Value, error) { return string(pp), nil }
-func (pp *parentPW) Scan(v interface{}) (err error) { *pp = parentPW(v.(string)); return }
-func (pp parentPW) KeyName() string { return "parent_pw" }
+type ParentPW string
+func (pp ParentPW) Value() (driver.Value, error) { return string(pp), nil }
+func (pp *ParentPW) Scan(src interface{}) (err error) { *pp = ParentPW(src.(string)); return }
+func (pp ParentPW) KeyName() string { return "parent_pw" }

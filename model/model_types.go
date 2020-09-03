e70f012f4f2b5b3e5285eb3140b0b0192ac799b3
지농 <jinhong0719@naver.com>
2020-09-03 22:54:15 +0900

    [MODIFY] <model/model_types.go> 모든 타입들의 접근 지정자 private으로 변경 및 그에 따른 생성자 추가

--- model/model_types.go
+++ model/model_types.go
@@ -5,113 +5,129 @@ import (
 )
 
 // Grade 필드에서 사용할 사용자 정의 타입
-type Grade int64
-func (g Grade) Value() (value driver.Value, err error) {
+type grade int64
+func Grade(i int64) grade { return grade(i) }
+func (g grade) Value() (value driver.Value, err error) {
 	value = int64(g)
 	if value == 0 { value = nil }
 	return
 }
-func (g *Grade) Scan(src interface{}) (_ error) { *g = Grade(src.(int64)); return }
-func (g Grade) KeyName() string { return "grade" }
+func (g *grade) Scan(src interface{}) (_ error) { *g = grade(src.(int64)); return }
+func (g grade) KeyName() string { return "grade" }
 
 // Class 필드에서 사용할 사용자 정의 타입
-type Class int64
-func (c Class) Value() (value driver.Value, err error) {
+type class int64
+func Class(i int64) class { return class(i) }
+func (c class) Value() (value driver.Value, err error) {
 	value = int64(c)
 	if value == 0 { value = nil }
 	return
 }
-func (c *Class) Scan(src interface{}) (err error) { *c = Class(src.(int64)); return }
-func (c Class) KeyName() string { return "class" }
+func (c *class) Scan(src interface{}) (err error) { *c = class(src.(int64)); return }
+func (c class) KeyName() string { return "class" }
 
 // StudentNumber 필드에서 사용할 사용자 정의 타입
-type StudentNumber int64
-func (sn StudentNumber) Value() (value driver.Value, err error) {
+type studentNumber int64
+func StudentNumber(i int64) studentNumber { return studentNumber(i) }
+func (sn studentNumber) Value() (value driver.Value, err error) {
 	value = int64(sn)
 	if value == 0 { value = nil }
 	return
 }
-func (sn *StudentNumber) Scan(src interface{}) (err error) { *sn = StudentNumber(src.(int64)); return }
-func (sn StudentNumber) KeyName() string { return "student_number" }
+func (sn *studentNumber) Scan(src interface{}) (err error) { *sn = studentNumber(src.(int64)); return }
+func (sn studentNumber) KeyName() string { return "student_number" }
 
 // UUID 필드에서 사용할 사용자 정의 타입
-type UUID string
-func (u UUID) Value() (driver.Value, error) { return string(u), nil }
-func (u *UUID) Scan(src interface{}) (err error) { *u = UUID(src.(string)); return }
-func (u UUID) KeyName() string { return "UUID" }
+type uuid string
+func UUID(s string) uuid { return uuid(s) }
+func (u uuid) Value() (driver.Value, error) { return string(u), nil }
+func (u *uuid) Scan(src interface{}) (err error) { *u = uuid(src.(string)); return }
+func (u uuid) KeyName() string { return "UUID" }
 
 // StudentID 필드에서 사용할 사용자 정의 타입
-type StudentID string
-func (si StudentID) Value() (driver.Value, error) { return string(si), nil }
-func (si *StudentID) Scan(src interface{}) (err error) { *si = StudentID(src.(string)); return }
-func (si StudentID) KeyName() string { return "student_id" }
+type studentID string
+func StudentID(s string) studentID { return studentID(s) }
+func (si studentID) Value() (driver.Value, error) { return string(si), nil }
+func (si *studentID) Scan(src interface{}) (err error) { *si = studentID(src.(string)); return }
+func (si studentID) KeyName() string { return "student_id" }
 
 // StudentPW 필드에서 사용할 사용자 정의 타입
-type StudentPW string
-func (sp StudentPW) Value() (driver.Value, error) { return string(sp), nil }
-func (sp *StudentPW) Scan(src interface{}) (err error) { *sp = StudentPW(src.(string)); return }
-func (sp StudentPW) KeyName() string { return "student_pw" }
+type studentPW string
+func StudentPW(s string) studentPW { return studentPW(s) }
+func (sp studentPW) Value() (driver.Value, error) { return string(sp), nil }
+func (sp *studentPW) Scan(src interface{}) (err error) { *sp = studentPW(src.(string)); return }
+func (sp studentPW) KeyName() string { return "student_pw" }
 
 // TeacherID 필드에서 사용할 사용자 정의 타입
-type TeacherID string
-func (ti TeacherID) Value() (driver.Value, error) { return string(ti), nil }
-func (ti *TeacherID) Scan(src interface{}) (err error) { *ti = TeacherID(src.(string)); return }
-func (ti TeacherID) KeyName() string { return "teacher_id" }
+type teacherID string
+func TeacherID(s string) teacherID { return teacherID(s) }
+func (ti teacherID) Value() (driver.Value, error) { return string(ti), nil }
+func (ti *teacherID) Scan(src interface{}) (err error) { *ti = teacherID(src.(string)); return }
+func (ti teacherID) KeyName() string { return "teacher_id" }
 
 // TeacherPW 필드에서 사용할 사용자 정의 타입
-type TeacherPW string
-func (tp TeacherPW) Value() (driver.Value, error) { return string(tp), nil }
-func (tp *TeacherPW) Scan(src interface{}) (err error) { *tp = TeacherPW(src.(string)); return }
-func (tp TeacherPW) KeyName() string { return "teacher_pw" }
+type teacherPW string
+func TeacherPW(s string) teacherPW { return teacherPW(s) }
+func (tp teacherPW) Value() (driver.Value, error) { return string(tp), nil }
+func (tp *teacherPW) Scan(src interface{}) (err error) { *tp = teacherPW(src.(string)); return }
+func (tp teacherPW) KeyName() string { return "teacher_pw" }
 
 // ParentID 필드에서 사용할 사용자 정의 타입
-type ParentID string
-func (pi ParentID) Value() (driver.Value, error) { return string(pi), nil }
-func (pi *ParentID) Scan(src interface{}) (err error) { *pi = ParentID(src.(string)); return }
-func (pi ParentID) KeyName() string { return "parent_id" }
+type parentID string
+func ParentID(s string) parentID { return parentID(s) }
+func (pi parentID) Value() (driver.Value, error) { return string(pi), nil }
+func (pi *parentID) Scan(src interface{}) (err error) { *pi = parentID(src.(string)); return }
+func (pi parentID) KeyName() string { return "parent_id" }
 
 // ParentPW 필드에서 사용할 사용자 정의 타입
-type ParentPW string
-func (pp ParentPW) Value() (driver.Value, error) { return string(pp), nil }
-func (pp *ParentPW) Scan(src interface{}) (err error) { *pp = ParentPW(src.(string)); return }
-func (pp ParentPW) KeyName() string { return "parent_pw" }
+type parentPW string
+func ParentPW(s string) parentPW { return parentPW(s) }
+func (pp parentPW) Value() (driver.Value, error) { return string(pp), nil }
+func (pp *parentPW) Scan(src interface{}) (err error) { *pp = parentPW(src.(string)); return }
+func (pp parentPW) KeyName() string { return "parent_pw" }
 
 // StudentUUID 필드에서 사용할 사용자 정의 타입
-type StudentUUID string
-func (su StudentUUID) Value() (driver.Value, error) { return string(su), nil }
-func (su *StudentUUID) Scan(src interface{}) (err error) { *su = StudentUUID(src.(string)); return }
-func (su StudentUUID) KeyName() string { return "student_uuid" }
+type studentUUID string
+func StudentUUID(s string) studentUUID { return studentUUID(s) }
+func (su studentUUID) Value() (driver.Value, error) { return string(su), nil }
+func (su *studentUUID) Scan(src interface{}) (err error) { *su = studentUUID(src.(string)); return }
+func (su studentUUID) KeyName() string { return "student_uuid" }
 
 // TeacherUUID 필드에서 사용할 사용자 정의 타입
-type TeacherUUID string
-func (tu TeacherUUID) Value() (driver.Value, error) { return string(tu), nil }
-func (tu *TeacherUUID) Scan(src interface{}) (err error) { *tu = TeacherUUID(src.(string)); return }
-func (tu TeacherUUID) KeyName() string { return "teacher_uuid" }
+type teacherUUID string
+func TeacherUUID(s string) teacherUUID { return teacherUUID(s) }
+func (tu teacherUUID) Value() (driver.Value, error) { return string(tu), nil }
+func (tu *teacherUUID) Scan(src interface{}) (err error) { *tu = teacherUUID(src.(string)); return }
+func (tu teacherUUID) KeyName() string { return "teacher_uuid" }
 
 // ParentUUID 필드에서 사용할 사용자 정의 타입
-type ParentUUID string
-func (pu ParentUUID) Value() (value driver.Value, err error) {
+type parentUUID string
+func ParentUUID(s string) parentUUID { return parentUUID(s) }
+func (pu parentUUID) Value() (value driver.Value, err error) {
 	value = string(pu)
 	if value == "" { value = nil }
 	return
 }
-func (pu *ParentUUID) Scan(src interface{}) (err error) { *pu = ParentUUID(src.(string)); return }
-func (pu ParentUUID) KeyName() string { return "parent_uuid" }
+func (pu *parentUUID) Scan(src interface{}) (err error) { *pu = parentUUID(src.(string)); return }
+func (pu parentUUID) KeyName() string { return "parent_uuid" }
 
 // Name 필드에서 사용할 사용자 정의 타입
-type Name string
-func (n Name) Value() (driver.Value, error) { return string(n), nil }
-func (n *Name) Scan(src interface{}) (err error) { *n = Name(src.(string)); return }
-func (n Name) KeyName() string { return "name" }
+type name string
+func Name(s string) name { return name(s) }
+func (n name) Value() (driver.Value, error) { return string(n), nil }
+func (n *name) Scan(src interface{}) (err error) { *n = name(src.(string)); return }
+func (n name) KeyName() string { return "name" }
 
 // PhoneNumber 필드에서 사용할 사용자 정의 타입
-type PhoneNumber string
-func (pn PhoneNumber) Value() (driver.Value, error) { return string(pn), nil }
-func (pn *PhoneNumber) Scan(src interface{}) (err error) { *pn = PhoneNumber(src.(string)); return }
-func (pn PhoneNumber) KeyName() string { return "phone_number" }
+type phoneNumber string
+func PhoneNumber(s string) phoneNumber { return phoneNumber(s) }
+func (pn phoneNumber) Value() (driver.Value, error) { return string(pn), nil }
+func (pn *phoneNumber) Scan(src interface{}) (err error) { *pn = phoneNumber(src.(string)); return }
+func (pn phoneNumber) KeyName() string { return "phone_number" }
 
 // ProfileURI 필드에서 사용할 사용자 정의 타입
-type ProfileURI string
-func (pu ProfileURI) Value() (driver.Value, error) { return string(pu), nil }
-func (pu *ProfileURI) Scan(src interface{}) (err error) { *pu = ProfileURI(src.(string)); return }
-func (pu ProfileURI) KeyName() string { return "profile_uri" }
+type profileURI string
+func ProfileURI(s string) profileURI { return profileURI(s) }
+func (pu profileURI) Value() (driver.Value, error) { return string(pu), nil }
+func (pu *profileURI) Scan(src interface{}) (err error) { *pu = profileURI(src.(string)); return }
+func (pu profileURI) KeyName() string { return "profile_uri" }

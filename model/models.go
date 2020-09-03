106d64eec2e16d58240283540c99d638cbac6aae
지농 <jinhong0719@naver.com>
2020-09-03 22:16:01 +0900

    [MODIFY] <model/model.go> 계정 관련 모델의 필드들 위에서 선언한 사용자 지정 타입으로 타입 변경

--- model/models.go
+++ model/models.go
@@ -8,10 +8,10 @@ import (
 // 학생 계정 테이블
 type StudentAuth struct {
 	gorm.Model
-	UUID       string `gorm:"PRIMARY_KEY;Type:char(20);UNIQUE;INDEX" validate:"uuid=student,len=20"` // 형식 => 'student-' + 12자리 랜덤 수 (20자)
-	StudentId  string `gorm:"Type:varchar(20);NOT NULL;UNIQUE" validate:"min=4,max=20,ascii"`        // 4~20자 사이
-	StudentPw  string `gorm:"Type:varchar(100);NOT NULL"`
-	ParentUUID string `gorm:"Type:char(19);" validate:"uuid=parent,len=19"` // 형식 => 'parent-' + 12자리 랜덤 수 (19자) 만일의 경우를 대비해서 NOT NULL 삭제
+	UUID       uuid      `gorm:"PRIMARY_KEY;Type:char(20);UNIQUE;INDEX" validate:"uuid=student,len=20"` // 형식 => 'student-' + 12자리 랜덤 수 (20자)
+	StudentID  studentID `gorm:"Type:varchar(20);NOT NULL;UNIQUE" validate:"min=4,max=20,ascii"`        // 4~20자 사이
+	StudentPW  studentPW `gorm:"Type:varchar(100);NOT NULL"`
+	ParentUUID string    `gorm:"Type:char(19);" validate:"uuid=parent,len=19"` // 형식 => 'parent-' + 12자리 랜덤 수 (19자) 만일의 경우를 대비해서 NOT NULL 삭제
 }
 
 // 학생 사용자 정보 테이블
@@ -26,14 +26,12 @@ type StudentInform struct {
 	ProfileUri    string        `gorm:"Type:varchar(150);UNIQUE;NOT NULL"`                            // 제약 조건 나중에 추가 예정
 }
 
-// ---
-
 // 선생님 계정 테이블
 type TeacherAuth struct {
 	gorm.Model
-	UUID      string `gorm:"PRIMARY_KEY;Type:char(20);UNIQUE;INDEX" validate:"uuid=teacher,len=20"` // 형식 => 'teacher-' + 12자리 랜덤 수 (20자)
-	TeacherId string `gorm:"varchar(20);NOT NULL;UNIQUE" validate:"min=4,max=20,ascii"`             // 4~20자 사이
-	TeacherPw string `gorm:"varchar(100):NOT NULL;"`
+	UUID      uuid      `gorm:"PRIMARY_KEY;Type:char(20);UNIQUE;INDEX" validate:"uuid=teacher,len=20"` // 형식 => 'teacher-' + 12자리 랜덤 수 (20자)
+	TeacherID teacherID `gorm:"varchar(20);NOT NULL;UNIQUE" validate:"min=4,max=20,ascii"`             // 4~20자 사이
+	TeacherPW teacherPW `gorm:"varchar(100):NOT NULL;"`
 }
 
 // 선생님 사용자 정보 테이블
@@ -41,19 +39,17 @@ type TeacherInform struct {
 	gorm.Model
 	TeacherUUID string `gorm:"Type:char(20);UNIQUE;NOT NULL'" validate:"uuid=teacher,len=20"` // 형식 => 'teacher-' + 12자리 랜덤 수 (20자)
 	Name        string `gorm:"Type:varchar(4);NOT NULL" validate:"min=2,max=4,korean"`        // 2~4자 사이 한글
-	Grade       grade  `gorm:"Type:tinyint(1);" validate:"range=0~3"`						  // in (1~3)
-	Class       class  `gorm:"Type:tinyint(1);" validate:"range=0~4"`						  // in (1~4)
+	Grade       grade  `gorm:"Type:tinyint(1);" validate:"range=0~3"`                         // in (1~3)
+	Class       class  `gorm:"Type:tinyint(1);" validate:"range=0~4"`                         // in (1~4)
 	PhoneNumber string `gorm:"Type:char(11);UNIQUE;NOT NULL" validate:"len=11,phone_number"`  // 11자
 }
-
-// ---
 
 // 부모님 계정 테이블
 type ParentAuth struct {
 	gorm.Model
-	UUID     string `gorm:"PRIMARY_KEY;Type:char(19);UNIQUE;INDEX" validate:"uuid=parent,len=19"` // 형식 => 'parent-' + 12자리 랜덤 수 (19자)
-	ParentId string `gorm:"Type:varchar(20);NOT NULL;UNIQUE" validate:"min=4,max=20,ascii"`       // 4~20자 사이
-	ParentPw string `gorm:"Type:varchar(100);NOT NULL"`
+	UUID     uuid     `gorm:"PRIMARY_KEY;Type:char(19);UNIQUE;INDEX" validate:"uuid=parent,len=19"` // 형식 => 'parent-' + 12자리 랜덤 수 (19자)
+	ParentID parentID `gorm:"Type:varchar(20);NOT NULL;UNIQUE" validate:"min=4,max=20,ascii"`       // 4~20자 사이
+	ParentPW parentPW `gorm:"Type:varchar(100);NOT NULL"`
 }
 
 // 부모님 사용자 정보 테이블

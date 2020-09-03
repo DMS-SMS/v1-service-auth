96a5948bcfa1e956f83b833caf716e6ee7f11181
지농 <jinhong0719@naver.com>
2020-09-03 22:02:25 +0900

    [ADD] <model/custom_types.go> 계정 관련 모델의 필드에 사용할 사용자 지정 타입 선언 및 특정 메서드 추가

--- model/custom_types.go
+++ model/custom_types.go
@@ -31,3 +31,38 @@ func (sn studentNumber) Value() (value driver.Value, err error) {
 }
 func (sn *studentNumber) Scan(v interface{}) (err error) { *sn = studentNumber(v.(int64)); return }
 func (sn studentNumber) KeyName() string { return "student_number" }
+
+type uuid string
+func (u uuid) Value() (driver.Value, error) { return string(u), nil }
+func (u *uuid) Scan(v interface{}) (err error) { *u = uuid(v.(string)); return }
+func (u uuid) KeyName() string { return "uuid" }
+
+type studentID string
+func (si studentID) Value() (driver.Value, error) { return string(si), nil }
+func (si *studentID) Scan(v interface{}) (err error) { *si = studentID(v.(string)); return }
+func (si studentID) KeyName() string { return "student_id" }
+
+type studentPW string
+func (sp studentPW) Value() (driver.Value, error) { return string(sp), nil }
+func (sp *studentPW) Scan(v interface{}) (err error) { *sp = studentPW(v.(string)); return }
+func (sp studentPW) KeyName() string { return "student_pw" }
+
+type teacherID string
+func (ti teacherID) Value() (driver.Value, error) { return string(ti), nil }
+func (ti *teacherID) Scan(v interface{}) (err error) { *ti = teacherID(v.(string)); return }
+func (ti teacherID) KeyName() string { return "teacher_id" }
+
+type teacherPW string
+func (tp teacherPW) Value() (driver.Value, error) { return string(tp), nil }
+func (tp *teacherPW) Scan(v interface{}) (err error) { *tp = teacherPW(v.(string)); return }
+func (tp teacherPW) KeyName() string { return "teacher_pw" }
+
+type parentID string
+func (pi parentID) Value() (driver.Value, error) { return string(pi), nil }
+func (pi *parentID) Scan(v interface{}) (err error) { *pi = parentID(v.(string)); return }
+func (pi parentID) KeyName() string { return "parent_id" }
+
+type parentPW string
+func (pp parentPW) Value() (driver.Value, error) { return string(pp), nil }
+func (pp *parentPW) Scan(v interface{}) (err error) { *pp = parentPW(v.(string)); return }
+func (pp parentPW) KeyName() string { return "parent_pw" }

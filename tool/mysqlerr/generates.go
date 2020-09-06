d49507d111a01d287343ebc5af1531b2a3cca2c3
지농 <jinhong0719@naver.com>
2020-09-06 23:26:21 +0900

    [MODIFY] <tool/mysqlerr/generates.go> FK Constraint 에러 Reference 관련 메시지 삭제

--- tool/mysqlerr/generates.go
+++ tool/mysqlerr/generates.go
@@ -19,10 +19,10 @@ type Reference struct {
 
 func FKConstraintFail(dbName, tableName, constraintName, attrName string, ref Reference) *mysql.MySQLError {
 	prefix := "Cannot add or update a child row: a foreign key constraint fails"
-	suffix := fmt.Sprintf("(`%s`.`%s`, CONSTRAINT `%s` FOREIGN KEY (`%s`) REFERENCES `%s` (`%s`) ON DELETE RESTRICT ON UPDATE)",
+	suffix := fmt.Sprintf("(`%s`.`%s`, CONSTRAINT `%s` FOREIGN KEY (`%s`) REFERENCES `%s` (`%s`))",
 		dbName, tableName, constraintName, attrName, ref.TableName, ref.AttrName)
 	return &mysql.MySQLError{
 		Number:  mysqlerr.ER_NO_REFERENCED_ROW_2,
-		Message: prefix + " " + suffix,
+		Message: fmt.Sprintf("%s %s", prefix, suffix),
 	}
 }

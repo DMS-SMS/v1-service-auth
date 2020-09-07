327f0af1cdfee1b96c7285844de6805e044e63ed
지농 <jinhong0719@naver.com>
2020-09-07 12:59:39 +0900

    [MODIFY] <tool/mysqlerr/generate.go> FKInform 테이블의 필드들 접근 제한자 pub으로 변경

--- tool/mysqlerr/generate.go
+++ tool/mysqlerr/generate.go
@@ -18,13 +18,13 @@ type RefInform struct {
 }
 
 type FKInform struct {
-	dbName, tableName, constraintName, attrName string
+	DBName, TableName, ConstraintName, AttrName string
 }
 
 func FKConstraintFail(fk FKInform, ref RefInform) *mysql.MySQLError {
 	prefix := "Cannot add or update a child row: a foreign key constraint fails"
 	suffix := fmt.Sprintf("(`%s`.`%s`, CONSTRAINT `%s` FOREIGN KEY (`%s`) REFERENCES `%s` (`%s`))",
-		fk.dbName, fk.tableName, fk.constraintName, fk.attrName, ref.TableName, ref.AttrName)
+		fk.DBName, fk.TableName, fk.ConstraintName, fk.AttrName, ref.TableName, ref.AttrName)
 	return &mysql.MySQLError{
 		Number:  mysqlerr.ER_NO_REFERENCED_ROW_2,
 		Message: fmt.Sprintf("%s %s", prefix, suffix),

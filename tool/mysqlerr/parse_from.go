08ae7c5c3eb2e922388a0ea3d225a23882950237
지농 <jinhong0719@naver.com>
2020-09-07 13:04:15 +0900

    [ADD] <tool/mysqlerr/parse_from.go> FK Constraint Fail 에러 파싱 및 반환 함수 추가

--- tool/mysqlerr/parse_from.go
+++ tool/mysqlerr/parse_from.go
@@ -1,1 +1,48 @@
 package mysqlerr
+
+import (
+	"errors"
+	"github.com/VividCortex/mysqlerr"
+	"github.com/go-sql-driver/mysql"
+	"regexp"
+	"strings"
+)
+
+const (
+	dbNameIndex = iota
+	tableNameIndex
+	constraintNameIndex
+	attrNameIndex
+	refTableNameIndex
+	refAttrNameIndex
+)
+
+func ParseFKConstraintFailErrorFrom(mysqlErr *mysql.MySQLError) (fk FKInform, ref RefInform, err error) {
+	if mysqlErr == nil || mysqlErr.Number != mysqlerr.ER_NO_REFERENCED_ROW_2 {
+		err = errors.New("parameter must be an FK Construct Fail Error")
+		return
+	}
+
+	regex := regexp.MustCompile("`.*?`")
+	matched := regex.FindAllString(mysqlErr.Message, -1)
+	for i := range matched {
+		matched[i] = strings.Trim(matched[i], "`")
+	}
+
+	if len(matched) != 6 {
+		err = errors.New("this parameter is incorrect for the FK Construct Fail Error format")
+		return
+	}
+
+	fk = FKInform{
+		DBName:         matched[dbNameIndex],
+		TableName:      matched[tableNameIndex],
+		ConstraintName: matched[constraintNameIndex],
+		AttrName:       matched[attrNameIndex],
+	}
+	ref = RefInform{
+		TableName: matched[refTableNameIndex],
+		AttrName:  matched[refAttrNameIndex],
+	}
+	return
+}

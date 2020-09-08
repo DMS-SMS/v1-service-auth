1ac3ff553d933d71dbc9af9153eb0b9736c2a4ae
지농 <jinhong0719@naver.com>
2020-09-08 09:52:33 +0900

    [ADD] <db/access/default_read.go> ID로 계정(학생, 선생님, 부모) 정보 조회 메서드 추가

--- db/access/default_read.go
+++ db/access/default_read.go
@@ -1,1 +1,21 @@
 package access
+
+import "auth/model"
+
+func (d *_default) GetStudentAuthWithID(id string) (auth *model.StudentAuth, err error) {
+	auth = new(model.StudentAuth)
+	err = d.tx.Where("student_id = ?", id).Find(&auth).Error
+	return
+}
+
+func (d *_default) GetTeacherAuthWithID(id string) (auth *model.TeacherAuth, err error) {
+	auth = new(model.TeacherAuth)
+	err = d.tx.Where("teacher_id = ?", id).Find(&auth).Error
+	return
+}
+
+func (d *_default) GetParentAuthWithID(id string) (auth *model.ParentAuth, err error) {
+	auth = new(model.ParentAuth)
+	err = d.tx.Where("parent_id = ?", id).Find(&auth).Error
+	return
+}

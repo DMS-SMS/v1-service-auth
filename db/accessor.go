063fee0291aabd901a7ac10d771342c18477c187
지농 <jinhong0719@naver.com>
2020-09-09 19:04:15 +0900

    [MODIFY] <db/accessor.go> 일부 메서드 매개변수 이름 수정 (sid, tid, pid -> uuid로 통합 등등..)

--- db/accessor.go
+++ db/accessor.go
@@ -7,24 +7,24 @@ import (
 
 type Accessor interface {
 	// 계정 생성 메서드
-	CreateStudentAuth(*model.StudentAuth) (result *model.StudentAuth, err error)
-	CreateTeacherAuth(*model.TeacherAuth) (result *model.TeacherAuth, err error)
-	CreateParentAuth(*model.ParentAuth) (result *model.ParentAuth, err error)
+	CreateStudentAuth(*model.StudentAuth) (auth *model.StudentAuth, err error)
+	CreateTeacherAuth(*model.TeacherAuth) (auth *model.TeacherAuth, err error)
+	CreateParentAuth(*model.ParentAuth) (auth *model.ParentAuth, err error)
 
 	// 계정 ID로 계정 정보 조회 메서드
-	GetStudentAuthWithID(id string) (*model.StudentAuth, error)
-	GetTeacherAuthWithID(id string) (*model.TeacherAuth, error)
-	GetParentAuthWithID(id string) (*model.ParentAuth, error)
+	GetStudentAuthWithID(studentID string) (*model.StudentAuth, error)
+	GetTeacherAuthWithID(teacherID string) (*model.TeacherAuth, error)
+	GetParentAuthWithID(parentID string) (*model.ParentAuth, error)
 
 	// 비밀번호 변경 메서드
-	ChangeStudentAuthPw(sid string, pw string) error
-	ChangeTeacherAuthPw(tid string, pw string) error
-	ChangeParentAuthPw(pid string, pw string) error
+	ChangeStudentAuthPw(uuid string, studentPW string) error
+	ChangeTeacherAuthPw(uuid string, teacherPW string) error
+	ChangeParentAuthPw(uuid string, parentPW string) error
 
 	// 계성 삭제 메서드 (Soft Delete)
-	DeleteStudentAuth(sid uint) error
-	DeleteTeacherAuth(tid string) error
-	DeleteParentAuth(pid string) error
+	DeleteStudentAuth(uuid uint) error
+	DeleteTeacherAuth(uuid string) error
+	DeleteParentAuth(uuid string) error
 
 	// ---
 
@@ -34,19 +34,19 @@ type Accessor interface {
 	CreateParentInform(*model.ParentInform) (result *model.ParentInform, err error)
 
 	// 사용자 정보로 uuid 조회 메서드 (계정 삭제 시 사용)
-	GetStudentUUIDsWithInform(*model.StudentInform) (sid []string, err error)
-	GetTeacherUUIDsWithInform(*model.TeacherInform) (tid []string, err error)
-	GetParentUUIDsWithInform(*model.ParentInform) (pid []string, err error)
+	GetStudentUUIDsWithInform(*model.StudentInform) (uuidArr []string, err error)
+	GetTeacherUUIDsWithInform(*model.TeacherInform) (uuidArr []string, err error)
+	GetParentUUIDsWithInform(*model.ParentInform) (uuidArr []string, err error)
 
 	// 계정 UUID로 정보 조회 메서드
-	GetStudentInformWithUUID(sid string) (*model.StudentInform, error)
-	GetTeacherInformWithUUID(tid string) (*model.TeacherInform, error)
-	GetParentInformWithUUID(pid string) (*model.ParentInform, error)
+	GetStudentInformWithUUID(uuid string) (*model.StudentInform, error)
+	GetTeacherInformWithUUID(uuid string) (*model.TeacherInform, error)
+	GetParentInformWithUUID(uuid string) (*model.ParentInform, error)
 
 	// 사용자 정보 수정 메서드
-	ModifyStudentInform(sid string, modify *model.StudentInform) (result *model.StudentInform, err error)
-	ModifyTeacherInform(tid string, modify *model.TeacherInform) (result *model.TeacherInform, err error)
-	ModifyParentInform(pid string, modify *model.ParentInform) (result *model.ParentInform, err error)
+	ModifyStudentInform(uuid string, modify *model.StudentInform) (result *model.StudentInform, err error)
+	ModifyTeacherInform(uuid string, modify *model.TeacherInform) (result *model.TeacherInform, err error)
+	ModifyParentInform(uuid string, modify *model.ParentInform) (result *model.ParentInform, err error)
 
 	// ---
 

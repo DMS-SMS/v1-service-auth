a96825feaa13674758fb84d2926a8097ccd4def5
지농 <jinhong0719@naver.com>
2020-09-09 20:18:49 +0900

    [ADD] <db/access/default_read.go> GetStudentUUIDsWithInform 메서드 구현 완료

--- db/access/default_read.go
+++ db/access/default_read.go
@@ -2,6 +2,12 @@ package access
 
 import (
 	"auth/model"
+	"github.com/jinzhu/gorm"
+)
+
+const (
+	emptyString = ""
+	emptyInt = 0
 )
 
 func (d *_default) GetStudentAuthWithID(studentID string) (auth *model.StudentAuth, err error) {
@@ -19,5 +25,44 @@ func (d *_default) GetTeacherAuthWithID(teacherID string) (auth *model.TeacherAu
 func (d *_default) GetParentAuthWithID(parentID string) (auth *model.ParentAuth, err error) {
 	auth = new(model.ParentAuth)
 	err = d.tx.Where("parent_id = ?", parentID).Find(&auth).Error
+	return
+}
+
+func (d *_default) GetStudentUUIDsWithInform(inform *model.StudentInform) (uuidArr []string, err error) {
+	cascadeTx := d.tx.New()
+
+	if inform.StudentUUID != emptyString {
+		cascadeTx = cascadeTx.Where("student_uuid = ?", inform.StudentUUID)
+	}
+	if inform.Grade != emptyInt {
+		cascadeTx = cascadeTx.Where("grade = ?", inform.Grade)
+	}
+	if inform.Class != emptyInt {
+		cascadeTx = cascadeTx.Where("class = ?", inform.Class)
+	}
+	if inform.StudentNumber != emptyInt {
+		cascadeTx = cascadeTx.Where("class = ?", inform.StudentNumber)
+	}
+	if inform.Name != emptyString {
+		cascadeTx = cascadeTx.Where("name = ?", inform.Name)
+	}
+	if inform.PhoneNumber != emptyString {
+		cascadeTx = cascadeTx.Where("phone_number = ?", inform.PhoneNumber)
+	}
+	if inform.ProfileURI != emptyString {
+		cascadeTx = cascadeTx.Where("profile_uri = ?", inform.ProfileURI)
+	}
+
+	informs := make([]*model.StudentInform, 1, 3)
+	err = cascadeTx.Find(&informs).Error
+
+	if len(informs) == 0 {
+		err = gorm.ErrRecordNotFound
+	}
+
+	for _, inform := range informs {
+		uuidArr = append(uuidArr, string(inform.StudentUUID))
+	}
+
 	return
 }

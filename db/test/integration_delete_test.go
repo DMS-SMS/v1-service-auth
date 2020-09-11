e72669af37550f34d9cefc8e6cd628b601949c26
지농 <jinhong0719@naver.com>
2020-09-11 22:19:36 +0900

    [ADD] <db/.../integration_delete_test.go> Delete(Teacher, Parent)Auth 테스트 함수 추가

--- db/test/integration_delete_test.go
+++ db/test/integration_delete_test.go
@@ -14,7 +14,7 @@ func Test_Access_DeleteStudentAuth(t *testing.T) {
 		log.Fatal(err)
 	}
 	defer func() {
-		access.Commit()
+		access.Rollback()
 		waitForFinish.Done()
 	}()
 
@@ -87,4 +87,102 @@ func Test_Access_DeleteStudentAuth(t *testing.T) {
 	//		ExpectError:        nil,
 	//	},
 	//} -> CheckIfStudentAuthExists 개발 후 적용
+}
+
+func Test_Access_DeleteTeacherAuth(t *testing.T) {
+	access, err := manager.BeginTx()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		access.Rollback()
+		waitForFinish.Done()
+	}()
+
+	// 선생님 계정 생성
+	for _, init := range []struct {
+		UUID, TeacherID, TeacherPW string
+	} {
+		{
+			UUID:      "teacher-111111111111",
+			TeacherID: "jinhong07191",
+			TeacherPW: passwords["testPW1"],
+		},
+	} {
+		_, err := access.CreateTeacherAuth(&model.TeacherAuth{
+			UUID:      model.UUID(init.UUID),
+			TeacherID: model.TeacherID(init.TeacherID),
+			TeacherPW: model.TeacherPW(init.TeacherPW),
+		})
+		if err != nil {
+			log.Fatal(fmt.Sprintf("error occurs while creating teacher auth, err: %v", err))
+		}
+	}
+
+	tests := []struct {
+		TeacherUUIDForArgs string
+		ExpectError        error
+	} {
+		{ // success case
+			TeacherUUIDForArgs: "teacher-111111111111",
+			ExpectError:        nil,
+		}, { // no exist student uuid -> not error!
+			TeacherUUIDForArgs: "teacher-222222222222",
+			ExpectError:        nil,
+		},
+	}
+
+	for _, test := range tests {
+		err := access.DeleteTeacherAuth(test.TeacherUUIDForArgs)
+		assert.Equalf(t, test.ExpectError, err, "error assertion error (test case: %v)", test)
+	}
+}
+
+func Test_Access_DeleteParentAuth(t *testing.T) {
+	access, err := manager.BeginTx()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer func() {
+		access.Rollback()
+		waitForFinish.Done()
+	}()
+
+	// 학부모 계정 생성
+	for _, init := range []struct {
+		UUID, ParentID, ParentPW string
+	} {
+		{
+			UUID:     "parent-111111111111",
+			ParentID: "jinhong07191",
+			ParentPW: passwords["testPW1"],
+		},
+	} {
+		_, err := access.CreateParentAuth(&model.ParentAuth{
+			UUID:     model.UUID(init.UUID),
+			ParentID: model.ParentID(init.ParentID),
+			ParentPW: model.ParentPW(init.ParentPW),
+		})
+		if err != nil {
+			log.Fatal(fmt.Sprintf("error occurs while creating parent auth, err: %v", err))
+		}
+	}
+
+	tests := []struct {
+		ParentUUIDForArgs string
+		ExpectError        error
+	} {
+		{ // success case
+			ParentUUIDForArgs: "parent-111111111111",
+			ExpectError:        nil,
+		}, { // no exist student uuid -> not error!
+			ParentUUIDForArgs: "parent-222222222222",
+			ExpectError:        nil,
+		},
+	}
+
+	for _, test := range tests {
+		err := access.DeleteParentAuth(test.ParentUUIDForArgs)
+		assert.Equalf(t, test.ExpectError, err, "error assertion error (test case: %v)", test)
+	}
 }

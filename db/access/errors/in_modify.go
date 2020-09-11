a12ba86e8f89d506cbf92a475bf7c5cd9d8b3a35
지농 <jinhong0719@naver.com>
2020-09-11 11:17:00 +0900

    [ADD] <db/access/errors/in_modify.go> teacher&parent uuid를 변경 X시 사용할 에러

--- db/access/errors/in_modify.go
+++ db/access/errors/in_modify.go
@@ -4,4 +4,6 @@ import "errors"
 
 var (
 	StudentUUIDCannotBeChanged = errors.New("student uuid cannot be changed")
+	TeacherUUIDCannotBeChanged = errors.New("teacher uuid cannot be changed")
+	ParentUUIDCannotBeChanged = errors.New("parent uuid cannot be changed")
 )

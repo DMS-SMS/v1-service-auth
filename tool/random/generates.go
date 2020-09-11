90fb568359cab8b2eff9b900e94fbdd4e657a640
지농 <jinhong0719@naver.com>
2020-09-11 11:58:13 +0900

    [ADD] <tool/random/generates.go> 여러 랜덤 값의 정수로 구성된 문자열 생성 및 반환 함수 추가

--- tool/random/generates.go
+++ tool/random/generates.go
@@ -1,1 +1,15 @@
 package random
+
+import "math/rand"
+
+var (
+	intLetters = []rune("0123456789")
+)
+
+func StringConsistOfIntWithLength(length int) string {
+	s := make([]rune, length)
+	for i := range s {
+		s[i] = intLetters[rand.Intn(len(intLetters))]
+	}
+	return string(s)
+}

4f081d258c7507731464255c61f9c7ed57bdb552
지농 <jinhong0719@naver.com>
2020-09-11 11:20:34 +0900

    [ADD] <db/access/default_update.go> ModifyTeacherInform 메서드 기능 구현 추가

--- db/access/default_update.go
+++ db/access/default_update.go
@@ -23,3 +23,20 @@ func (d *_default) ModifyStudentInform(uuid string, revisionInform *model.Studen
 	err = d.tx.Model(&model.StudentInform{}).Where("student_uuid = ?", uuid).Updates(contextForUpdate).Error
 	return
 }
+
+func (d *_default) ModifyTeacherInform(uuid string, revisionInform *model.TeacherInform) (err error) {
+	contextForUpdate := make(map[string]interface{}, 4)
+
+	if revisionInform.TeacherUUID != emptyString {
+		err = errors.TeacherUUIDCannotBeChanged
+		return
+	}
+
+	if revisionInform.Grade != emptyInt          { contextForUpdate[revisionInform.Grade.KeyName()] = revisionInform.Grade }
+	if revisionInform.Class != emptyInt          { contextForUpdate[revisionInform.Class.KeyName()] = revisionInform.Class }
+	if revisionInform.Name != emptyString        { contextForUpdate[revisionInform.Name.KeyName()] = revisionInform.Name }
+	if revisionInform.PhoneNumber != emptyString { contextForUpdate[revisionInform.PhoneNumber.KeyName()] = revisionInform.PhoneNumber }
+
+	err = d.tx.Model(&model.TeacherInform{}).Where("teacher_uuid = ?", uuid).Updates(contextForUpdate).Error
+	return
+}

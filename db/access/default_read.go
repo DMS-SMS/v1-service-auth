f4289e93940f7c26a6bbadc107f3b19e146c6a4d
지농 <jinhong0719@naver.com>
2020-09-11 10:15:18 +0900

    [MODIFY] <db/access/default_read.go> GetXXXUUIDsWithInform 메서드 코드 정렬 수정

--- db/access/default_read.go
+++ db/access/default_read.go
@@ -31,27 +31,13 @@ func (d *_default) GetParentAuthWithID(parentID string) (auth *model.ParentAuth,
 func (d *_default) GetStudentUUIDsWithInform(inform *model.StudentInform) (uuidArr []string, err error) {
 	cascadeTx := d.tx.New()
 
-	if inform.StudentUUID != emptyString {
-		cascadeTx = cascadeTx.Where("student_uuid = ?", inform.StudentUUID)
-	}
-	if inform.Grade != emptyInt {
-		cascadeTx = cascadeTx.Where("grade = ?", inform.Grade)
-	}
-	if inform.Class != emptyInt {
-		cascadeTx = cascadeTx.Where("class = ?", inform.Class)
-	}
-	if inform.StudentNumber != emptyInt {
-		cascadeTx = cascadeTx.Where("class = ?", inform.StudentNumber)
-	}
-	if inform.Name != emptyString {
-		cascadeTx = cascadeTx.Where("name = ?", inform.Name)
-	}
-	if inform.PhoneNumber != emptyString {
-		cascadeTx = cascadeTx.Where("phone_number = ?", inform.PhoneNumber)
-	}
-	if inform.ProfileURI != emptyString {
-		cascadeTx = cascadeTx.Where("profile_uri = ?", inform.ProfileURI)
-	}
+	if inform.StudentUUID != emptyString { cascadeTx = cascadeTx.Where("student_uuid = ?", inform.StudentUUID) }
+	if inform.Grade != emptyInt          { cascadeTx = cascadeTx.Where("grade = ?", inform.Grade) }
+	if inform.Class != emptyInt          { cascadeTx = cascadeTx.Where("class = ?", inform.Class) }
+	if inform.StudentNumber != emptyInt  { cascadeTx = cascadeTx.Where("class = ?", inform.StudentNumber) }
+	if inform.Name != emptyString        { cascadeTx = cascadeTx.Where("name = ?", inform.Name) }
+	if inform.PhoneNumber != emptyString { cascadeTx = cascadeTx.Where("phone_number = ?", inform.PhoneNumber) }
+	if inform.ProfileURI != emptyString  { cascadeTx = cascadeTx.Where("profile_uri = ?", inform.ProfileURI) }
 
 	informs := make([]*model.StudentInform, 1, 3)
 	err = cascadeTx.Find(&informs).Error
@@ -69,21 +55,11 @@ func (d *_default) GetStudentUUIDsWithInform(inform *model.StudentInform) (uuidA
 func (d *_default) GetTeacherUUIDsWithInform(inform *model.TeacherInform) (uuidArr []string, err error) {
 	cascadeTx := d.tx.New()
 
-	if inform.TeacherUUID != emptyString {
-		cascadeTx = cascadeTx.Where("teacher_uuid = ?", inform.TeacherUUID)
-	}
-	if inform.Grade != emptyInt {
-		cascadeTx = cascadeTx.Where("grade = ?", inform.Grade)
-	}
-	if inform.Class != emptyInt {
-		cascadeTx = cascadeTx.Where("class = ?", inform.Class)
-	}
-	if inform.Name != emptyString {
-		cascadeTx = cascadeTx.Where("name = ?", inform.Name)
-	}
-	if inform.PhoneNumber != emptyString {
-		cascadeTx = cascadeTx.Where("phone_number = ?", inform.PhoneNumber)
-	}
+	if inform.TeacherUUID != emptyString { cascadeTx = cascadeTx.Where("teacher_uuid = ?", inform.TeacherUUID) }
+	if inform.Grade != emptyInt          { cascadeTx = cascadeTx.Where("grade = ?", inform.Grade) }
+	if inform.Class != emptyInt          { cascadeTx = cascadeTx.Where("class = ?", inform.Class) }
+	if inform.Name != emptyString        { cascadeTx = cascadeTx.Where("name = ?", inform.Name) }
+	if inform.PhoneNumber != emptyString { cascadeTx = cascadeTx.Where("phone_number = ?", inform.PhoneNumber) }
 
 	informs := make([]*model.TeacherInform, 1, 3)
 	err = cascadeTx.Find(&informs).Error
@@ -101,15 +77,9 @@ func (d *_default) GetTeacherUUIDsWithInform(inform *model.TeacherInform) (uuidA
 func (d *_default) GetParentUUIDsWithInform(inform *model.ParentInform) (uuidArr []string, err error) {
 	cascadeTx := d.tx.New()
 
-	if inform.ParentUUID != emptyString {
-		cascadeTx = cascadeTx.Where("parent_uuid = ?", inform.ParentUUID)
-	}
-	if inform.Name != emptyString {
-		cascadeTx = cascadeTx.Where("name = ?", inform.Name)
-	}
-	if inform.PhoneNumber != emptyString {
-		cascadeTx = cascadeTx.Where("phone_number = ?", inform.PhoneNumber)
-	}
+	if inform.ParentUUID != emptyString  { cascadeTx = cascadeTx.Where("parent_uuid = ?", inform.ParentUUID) }
+	if inform.Name != emptyString        { cascadeTx = cascadeTx.Where("name = ?", inform.Name) }
+	if inform.PhoneNumber != emptyString { cascadeTx = cascadeTx.Where("phone_number = ?", inform.PhoneNumber) }
 
 	informs := make([]*model.ParentInform, 1, 3)
 	err = cascadeTx.Find(&informs).Error

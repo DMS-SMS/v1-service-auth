ff6f57ba02edb40a56cbb7b63e1913bf1c279a50
지농 <jinhong0719@naver.com>
2020-09-12 14:17:57 +0900

    [MODIFY] <handler/none.go> auth proto의 일부 서비스 및 메시지의 수정에 따른 None 객체 수정

--- handler/none.go
+++ handler/none.go
@@ -8,21 +8,21 @@ import (
 type None struct {}
 
 // About Admin RPC Service
-func(n None) CreateStudentAuth(context.Context, *proto.CreateStudentAuthRequest, *proto.CreateStudentAuthResponse) (err error) { return }
-func(n None) CreateTeacherAuth(context.Context, *proto.CreateTeacherAuthRequest, *proto.CreateTeacherAuthResponse) (err error) { return }
-func(n None) CreateParentAuth(context.Context, *proto.CreateParentAuthRequest, *proto.CreateParentAuthResponse) (err error) { return }
+func(n None) CreateNewAuth(context.Context, *proto.CreateNewAuthRequest, *proto.CreateNewAuthResponse) (err error) { return }
+func(n None) CreateNewTeacher(context.Context, *proto.CreateNewTeacherRequest, *proto.CreateNewTeacherResponse) (err error) { return }
+func(n None) CreateNewParent(context.Context, *proto.CreateNewParentRequest, *proto.CreateNewParentResponse) (err error) { return }
 
 // About Student RPC Service
 func(n None) LoginStudentAuth(context.Context, *proto.LoginStudentAuthRequest, *proto.LoginStudentAuthResponse) (err error) { return }
-func(n None) ChangeStudentAuthPw(context.Context, *proto.ChangeStudentAuthPwRequest, *proto.ChangeStudentAuthPwResponse) (err error) { return }
-func(n None) GetStudentUserInform(context.Context, *proto.GetStudentUserInformRequest, *proto.GetStudentUserInformResponse) (err error) { return }
+func(n None) ChangeStudentPW(context.Context, *proto.ChangeStudentPWRequest, *proto.ChangeStudentPWResponse) (err error) { return }
+func(n None) GetStudentInformWithUUID(context.Context, *proto.GetStudentInformWithUUIDRequest, *proto.GetStudentInformWithUUIDResponse) (err error) { return }
 
 // About Teacher RPC Service
 func(n None) LoginTeacherAuth(context.Context, *proto.LoginTeacherAuthRequest, *proto.LoginTeacherAuthResponse) (err error) { return }
-func(n None) ChangeTeacherAuthPw(context.Context, *proto.ChangeTeacherAuthPwRequest, *proto.ChangeTeacherAuthPwResponse) (err error) { return }
-func(n None) GetTeacherUserInform(context.Context, *proto.GetTeacherUserInformRequest, *proto.GetTeacherUserInformResponse) (err error) { return }
+func(n None) ChangeTeacherPW(context.Context, *proto.ChangeTeacherPWRequest, *proto.ChangeTeacherPWResponse) (err error) { return }
+func(n None) GetTeacherInformWithUUID(context.Context, *proto.GetTeacherInformWithUUIDRequest, *proto.GetTeacherInformWithUUIDResponse) (err error) { return }
 
 // About Parent RPC Service
 func(n None) LoginParentAuth(context.Context, *proto.LoginParentAuthRequest, *proto.LoginParentAuthResponse) (err error) { return }
-func(n None) ChangeParentAuthPw(context.Context, *proto.ChangeParentAuthPwRequest, *proto.ChangeParentAuthPwResponse) (err error) { return }
-func(n None) GetParentUserInform(context.Context, *proto.GetParentUserInformRequest, *proto.GetParentUserInformResponse) (err error) { return }
+func(n None) ChangeParentPW(context.Context, *proto.ChangeParentPWRequest, *proto.ChangeParentPWResponse) (err error) { return }
+func(n None) GetParentInformWithUUID(context.Context, *proto.GetParentInformWithUUIDRequest, *proto.GetParentInformWithUUIDResponse) (err error) { return }

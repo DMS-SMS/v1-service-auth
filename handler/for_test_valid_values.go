2e89f1ad1b836e49937e344f492ba4e74110b6fa
지농 <jinhong0719@naver.com>
2020-09-13 14:12:24 +0900

    [ADD] <handler/for_test_valid_values.go> 에러 발샐 후 패닉 시, 패닉 내용에 상황 설명 추가

--- handler/for_test_valid_values.go
+++ handler/for_test_valid_values.go
@@ -30,10 +30,10 @@ var (
 
 func init() {
 	exampleTracerForAPIGateway, closer, err := jaegercfg.Configuration{ServiceName: "DMS.SMS.v1.api.gateway"}.NewTracer()
-	if err != nil { log.Fatal(err) }
+	if err != nil { log.Fatal(fmt.Sprintf("error while creating new tracer for api, err: %v", err)) }
 	defer func() { _ = closer.Close() }()
 	exampleTracerForRPCService, closer, err := jaegercfg.Configuration{ServiceName: "DMS.SMS.v1.service.auth"}.NewTracer()
-	if err != nil { log.Fatal(err) }
+	if err != nil { log.Fatal(fmt.Sprintf("error while creating new tracer for service, err: %v", err)) }
 	defer func() { _ = closer.Close() }()
 
 	exampleSpanForAPIGateway := exampleTracerForAPIGateway.StartSpan("v1/students")
@@ -41,13 +41,13 @@ func init() {
 	validSpanContextString = exampleSpanForRPCService.Context().(jaeger.SpanContext).String()
 
 	absPath, err := filepath.Abs("./images_for_test/doraemon.png")
-	if err != nil { log.Fatal(err) }
+	if err != nil { log.Fatal(fmt.Sprintf("error while getting abstract file path, err: %v", err)) }
 	file, err := os.Open(absPath)
-	if err != nil { log.Fatal(err) }
+	if err != nil { log.Fatal(fmt.Sprintf("error while opening new test image files, err: %v", err)) }
 	fileInfo, err := file.Stat()
-	if err != nil { log.Fatal(err) }
+	if err != nil { log.Fatal(fmt.Sprintf("error while getting file information, err: %v", err)) }
 	validImageByteArr = make([]byte, fileInfo.Size())
 	fileReader := bufio.NewReader(file)
 	_, err = fileReader.Read(validImageByteArr)
-	if err != nil { log.Fatal(err) }
+	if err != nil { log.Fatal(fmt.Sprintf("error while reading from image file, err: %v", err)) }
 }

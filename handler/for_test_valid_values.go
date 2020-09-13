913ae4cfd4be5ad951f1356d19d19f9872bf9b6c
지농 <jinhong0719@naver.com>
2020-09-13 13:58:36 +0900

    [ADD] <handler/for_test_valid_values.go> CreateNewStudent 관련 valid 변수&상수 선언

--- handler/for_test_valid_values.go
+++ handler/for_test_valid_values.go
@@ -1,1 +1,18 @@
 package handler
+
+const (
+	validAdminUUID = "admin-111111111111"
+	validParentUUID = "parent-111111111111"
+	validStudentID = "jinhong0719"
+	validStudentPW = "testPW"
+	validGrade = 2
+	validClass = 2
+	validStudentNumber = 7
+	validName = "박진홍"
+	validPhoneNumber = "01088378347"
+)
+
+var (
+	validImageByteArr []byte
+	validSpanContextString string
+)

f38368479efef338edb73c8030d35d8e480cdad9
지농 <jinhong0719@naver.com>
2020-09-13 19:42:19 +0900

    [ADD] <handler/for_test_global_variables.go> 핸들러, Mock 객체 선언 및 초기화 함수 추가

--- handler/for_test_global_variables.go
+++ handler/for_test_global_variables.go
@@ -1,1 +1,32 @@
 package handler
+
+import (
+	"auth/db"
+	"auth/db/access"
+	"fmt"
+	"github.com/stretchr/testify/mock"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+	"log"
+	//proto "auth/proto/golang/auth"
+)
+
+var (
+	defaultHandler *_default
+	mockForAccessor *mock.Mock
+)
+
+func init() {
+	mockForAccessor = new(mock.Mock)
+
+	exampleTracerForRPCService, closer, err := jaegercfg.Configuration{ServiceName: "DMS.SMS.v1.service.auth"}.NewTracer()
+	if err != nil { log.Fatal(fmt.Sprintf("error while creating new tracer for service, err: %v", err)) }
+	defer func() { _ = closer.Close() }()
+
+	mockAccessManager, err := db.NewAccessorManage(access.Mock(mockForAccessor))
+	if err != nil { log.Fatal(fmt.Sprintf("error while creating new access manage with mock, err: %v", err)) }
+
+	defaultHandler = &_default{
+		manager: mockAccessManager,
+		tracer:  exampleTracerForRPCService,
+	}
+}

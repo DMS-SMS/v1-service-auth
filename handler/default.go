908b02cbb2a764772f5c378425dee6988d68767a
지농 <jinhong0719@naver.com>
2020-09-13 11:08:26 +0900

    [ADD] <handler/default.go> Field Setter 함수를 넘겨주는 방식으로 초기화하는 생성자 추가

--- handler/default.go
+++ handler/default.go
@@ -10,3 +10,23 @@ type _default struct {
 	manager db.AccessorManage
 	tracer  opentracing.Tracer
 }
+
+type FieldSetter func(*_default)
+
+func Default(setters ...FieldSetter) *_default {
+	return newDefault(setters...)
+}
+
+func newDefault(setters ...FieldSetter) (h *_default) {
+	h = new(_default)
+	for _, setter := range setters {
+		setter(h)
+	}
+	return
+}
+
+func Manager(manager db.AccessorManage) FieldSetter {
+	return func(h *_default) {
+		h.manager = manager
+	}
+}

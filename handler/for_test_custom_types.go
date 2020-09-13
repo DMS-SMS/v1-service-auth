292c7c80d7d310bb06955ea6249a7c990d30d396
지농 <jinhong0719@naver.com>
2020-09-13 14:22:14 +0900

    [ADD] <handler/for_test_custom_types.go> CreateNewStudent 메서드 테스트용 구조체 추가

--- handler/for_test_custom_types.go
+++ handler/for_test_custom_types.go
@@ -2,3 +2,17 @@ package handler
 
 type method string
 type returns []interface{}
+
+type createNewStudentTest struct {
+	UUID                 string
+	StudentID, StudentPW string
+	ParentUUID           string
+	Grade, Class         uint32
+	StudentNumber        uint32
+	Name, PhoneNumber    string
+	Image                []byte
+	ExpectMethod         map[method]returns
+	ExpectedStatus       uint32
+	ExpectedCode         int32
+	ExpectedMessage      string
+}

6627c7efab840d4af125f1a9032364283455f120
지농 <jinhong0719@naver.com>
2020-09-13 14:00:21 +0900

    [ADD] <handler/for_test_valid_values.go> init 함수 선언 및 valid 변수에 값 대입 기능 구현

--- handler/for_test_valid_values.go
+++ handler/for_test_valid_values.go
@@ -1,5 +1,16 @@
 package handler
 
+import (
+	"bufio"
+	"fmt"
+	"github.com/opentracing/opentracing-go"
+	"github.com/uber/jaeger-client-go"
+	jaegercfg "github.com/uber/jaeger-client-go/config"
+	"log"
+	"os"
+	"path/filepath"
+)
+
 const (
 	validAdminUUID = "admin-111111111111"
 	validParentUUID = "parent-111111111111"
@@ -16,3 +27,27 @@ var (
 	validImageByteArr []byte
 	validSpanContextString string
 )
+
+func init() {
+	exampleTracerForAPIGateway, closer, err := jaegercfg.Configuration{ServiceName: "DMS.SMS.v1.api.gateway"}.NewTracer()
+	if err != nil { log.Fatal(err) }
+	defer func() { _ = closer.Close() }()
+	exampleTracerForRPCService, closer, err := jaegercfg.Configuration{ServiceName: "DMS.SMS.v1.service.auth"}.NewTracer()
+	if err != nil { log.Fatal(err) }
+	defer func() { _ = closer.Close() }()
+
+	exampleSpanForAPIGateway := exampleTracerForAPIGateway.StartSpan("v1/students")
+	exampleSpanForRPCService := exampleTracerForRPCService.StartSpan("CreateNewStudent", opentracing.ChildOf(exampleSpanForAPIGateway.Context()))
+	validSpanContextString = exampleSpanForRPCService.Context().(jaeger.SpanContext).String()
+
+	absPath, err := filepath.Abs("./images_for_test/doraemon.png")
+	if err != nil { log.Fatal(err) }
+	file, err := os.Open(absPath)
+	if err != nil { log.Fatal(err) }
+	fileInfo, err := file.Stat()
+	if err != nil { log.Fatal(err) }
+	validImageByteArr = make([]byte, fileInfo.Size())
+	fileReader := bufio.NewReader(file)
+	_, err = fileReader.Read(validImageByteArr)
+	if err != nil { log.Fatal(err) }
+}

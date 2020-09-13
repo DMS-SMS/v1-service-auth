a84ea8d235cf831c905228782189ee7e492c97ac
지농 <jinhong0719@naver.com>
2020-09-13 14:25:07 +0900

    [ADD] <handler/for_test_custom_types.go> 빈 값을 유효한 값으로 변경하는 메서드 선언 추가

--- handler/for_test_custom_types.go
+++ handler/for_test_custom_types.go
@@ -16,3 +16,16 @@ type createNewStudentTest struct {
 	ExpectedCode         int32
 	ExpectedMessage      string
 }
+
+func (test *createNewStudentTest) ChangeEmptyValueToValidValue() {
+	if test.UUID == emptyString          { test.UUID = validAdminUUID }
+	if test.StudentID == emptyString     { test.StudentID = validStudentID }
+	if test.StudentPW == emptyString     { test.StudentPW = validStudentPW }
+	if test.ParentUUID == emptyString    { test.ParentUUID = validParentUUID }
+	if test.Grade == emptyUint32         { test.Grade = validGrade }
+	if test.Class == emptyUint32         { test.Class = validClass }
+	if test.StudentNumber == emptyUint32 { test.StudentNumber = validStudentNumber }
+	if test.Name == emptyString          { test.Name = validName }
+	if test.PhoneNumber == emptyString   { test.PhoneNumber = validPhoneNumber }
+	if string(test.Image) == emptyString { test.Image = validImageByteArr }
+}

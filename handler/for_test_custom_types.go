75b785f521c22396032c66d2f11db0c09d646299
지농 <jinhong0719@naver.com>
2020-09-13 14:25:31 +0900

    [ADD] <handler/for_test_custom_types.go> 빈 값 대체 값을 빈 값으로 변경하는 메서드 선언 추가

--- handler/for_test_custom_types.go
+++ handler/for_test_custom_types.go
@@ -29,3 +29,16 @@ func (test *createNewStudentTest) ChangeEmptyValueToValidValue() {
 	if test.PhoneNumber == emptyString   { test.PhoneNumber = validPhoneNumber }
 	if string(test.Image) == emptyString { test.Image = validImageByteArr }
 }
+
+func (test *createNewStudentTest) ChangeEmptyReplaceValueToEmptyValue() {
+	if test.UUID == emptyReplaceValueForString               { test.UUID = "" }
+	if test.StudentID == emptyReplaceValueForString          { test.StudentID = "" }
+	if test.StudentPW == emptyReplaceValueForString          { test.StudentPW = "" }
+	if test.ParentUUID == emptyReplaceValueForString         { test.ParentUUID = "" }
+	if test.Grade == uint32(emptyReplaceValueForInt)         { test.Grade = 0 }
+	if test.Class == uint32(emptyReplaceValueForInt)         { test.Class = 0 }
+	if test.StudentNumber == uint32(emptyReplaceValueForInt) { test.StudentNumber = 0 }
+	if test.Name == emptyReplaceValueForString               { test.Name = "" }
+	if test.PhoneNumber == emptyReplaceValueForString        { test.PhoneNumber = "" }
+	if string(test.Image) == emptyReplaceValueForString	     { test.Image = []byte{} }
+}

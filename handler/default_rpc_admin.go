57d47fd18f9eb49fc709e09c1b7a1d39e066c8c3
지농 <jinhong0719@naver.com>
2020-09-14 20:16:07 +0900

    [ADD] <handler/default_rpc_admin.go> CreateNewStudent 407 관련 처리 로직 구현

--- handler/default_rpc_admin.go
+++ handler/default_rpc_admin.go
@@ -1,1 +1,53 @@
 package handler
+
+import (
+	proto "auth/proto/golang/auth"
+	"context"
+	"fmt"
+	"github.com/google/uuid"
+	"github.com/micro/go-micro/v2/metadata"
+	"github.com/uber/jaeger-client-go"
+	"net/http"
+)
+
+func(h _default) CreateNewStudent(ctx context.Context, req *proto.CreateNewStudentRequest, resp *proto.CreateNewStudentResponse) (_ error) {
+	const (
+		proxyAuthRequiredMessageFormat = "proxy auth required (reason: %s)"
+	)
+
+	md, ok := metadata.FromContext(ctx)
+	if !ok {
+		resp.Status = http.StatusProxyAuthRequired
+		resp.Message = fmt.Sprintf(proxyAuthRequiredMessageFormat, "metadata not exists")
+		return
+	}
+
+	reqID, ok := md.Get("X-Request-Id")
+	if !ok {
+		resp.Status = http.StatusProxyAuthRequired
+		resp.Message = fmt.Sprintf(proxyAuthRequiredMessageFormat, "X-Request-Id not exists")
+		return
+	}
+
+	_, err := uuid.Parse(reqID)
+	if err != nil {
+		resp.Status = http.StatusProxyAuthRequired
+		resp.Message = fmt.Sprintf(proxyAuthRequiredMessageFormat, "X-Request-ID invalid, err: " + err.Error())
+	}
+
+	spanCtx, ok := md.Get("Span-Context")
+	if !ok {
+		resp.Status = http.StatusProxyAuthRequired
+		resp.Message = fmt.Sprintf(proxyAuthRequiredMessageFormat, "Span-Context not exists")
+		return
+	}
+
+	parentSpan, err := jaeger.ContextFromString(spanCtx)
+	if err != nil {
+		resp.Status = http.StatusProxyAuthRequired
+		resp.Message = fmt.Sprintf(proxyAuthRequiredMessageFormat, "Span-Context invalid, err: " + err.Error())
+		return
+	}
+
+	return
+}

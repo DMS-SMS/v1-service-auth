129eaf5f3b49374a39caf2920bdd97402ae7abcb
지농 <jinhong0719@naver.com>
2020-09-15 19:36:47 +0900

    [ADD] <handler/default_utils.go> getContextFromMetadata 메서드 해당 파일에 선언

--- handler/default_rpc_admin.go
+++ handler/default_rpc_admin.go
@@ -13,8 +13,6 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 	"github.com/go-playground/validator/v10"
 	"github.com/go-sql-driver/mysql"
-	"github.com/google/uuid"
-	"github.com/micro/go-micro/v2/metadata"
 	"github.com/opentracing/opentracing-go"
 	"github.com/opentracing/opentracing-go/log"
 	"github.com/uber/jaeger-client-go"
@@ -224,56 +222,6 @@ func(h _default) CreateNewStudent(ctx context.Context, req *proto.CreateNewStude
 	resp.Status = http.StatusCreated
 	resp.Message = "new student create success"
 	resp.CreatedStudentUUID = sUUID
-
-	return
-}
-
-func (_ *_default) getContextFromMetadata(ctx context.Context) (parsedCtx context.Context, proxyAuthenticated bool, reason string) {
-	md, ok := metadata.FromContext(ctx)
-	if !ok {
-		proxyAuthenticated = false
-		reason = "metadata not exists"
-		return
-	}
-
-	reqID, ok := md.Get("X-Request-Id")
-	if !ok {
-		proxyAuthenticated = false
-		reason = "X-Request-Id not exists"
-		return
-	}
-
-	_, err := uuid.Parse(reqID)
-	if err != nil {
-		proxyAuthenticated = false
-		reason = "X-Request-ID invalid, err: " + err.Error()
-		return
-	}
-
-	spanCtx, ok := md.Get("Span-Context")
-	if !ok {
-		proxyAuthenticated = false
-		reason = "Span-Context not exists"
-		return
-	}
-
-	parentSpan, err := jaeger.ContextFromString(spanCtx)
-	if err != nil {
-		proxyAuthenticated = false
-		reason = "Span-Context invalid, err: " + err.Error()
-		return
-	}
-
-	proxyAuthenticated = true
-	reason = ""
-
-	parsedCtx = context.Background()
-	parsedCtx = context.WithValue(parsedCtx, "X-Request-Id", reqID)
-	parsedCtx = context.WithValue(parsedCtx, "Span-Context", parentSpan)
-
-	if sUUID, ok := md.Get("StudentUUID"); ok { parsedCtx = context.WithValue(parsedCtx, "StudentUUID", sUUID) }
-	if tUUID, ok := md.Get("TeacherUUID"); ok { parsedCtx = context.WithValue(parsedCtx, "TeacherUUID", tUUID) }
-	if pUUID, ok := md.Get("ParentUUID"); ok  { parsedCtx = context.WithValue(parsedCtx, "ParentUUID", pUUID) }
 
 	return
 }

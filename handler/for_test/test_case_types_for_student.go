af5d188f268042304f61eca3f740723efb1e1bbf
지농 <jinhong0719@naver.com>
2020-09-15 22:56:28 +0900

    [ADD] <handler/for_test/test_case_..._student.go> LoginStudentAuth 테스트 주도 객체 선언

--- handler/for_test/test_case_types_for_student.go
+++ handler/for_test/test_case_types_for_student.go
@@ -1,1 +1,13 @@
 package test
+
+type LoginStudentAuthCase struct {
+	StudentID, StudentPW        string
+	XRequestID                  string
+	SpanContextString           string
+	ExpectedMethods             map[Method]Returns
+	ExpectedStatus              uint32
+	ExpectedCode                int32
+	ExpectedMessage             string
+	ExpectedAccessToken			string
+	ExpectedLoggedInStudentUUID string
+}

572d2fc4384cffa2ea5b76e1a240a376df07859e
지농 <jinhong0719@naver.com>
2020-09-15 22:35:50 +0900

    [MODIFY] <handler/for_test/util_funcs.go> ../for_test_....go에서 util 함수 이동

--- handler/for_test_global_variables.go
+++ handler/for_test_global_variables.go
@@ -3,13 +3,10 @@ package handler
 import (
 	"auth/db"
 	"auth/db/access"
-	"auth/tool/random"
 	"fmt"
-	"github.com/jinzhu/gorm"
 	"github.com/stretchr/testify/mock"
 	jaegercfg "github.com/uber/jaeger-client-go/config"
 	"log"
-	"time"
 )
 
 var (
@@ -30,15 +27,5 @@ func init() {
 	defaultHandler = &_default{
 		accessManage: mockAccessManage,
 		tracer:       exampleTracerForRPCService,
-	}
-}
-
-func createGormModelOnCurrentTime() gorm.Model {
-	currentTime := time.Now()
-	return gorm.Model{
-		ID:        uint(random.Int64WithLength(3)),
-		CreatedAt: currentTime,
-		UpdatedAt: currentTime,
-		DeletedAt: nil,
 	}
 }

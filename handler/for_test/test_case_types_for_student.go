ef2087af672310c954c740ee1438e81e4e3840e4
지농 <jinhong0719@naver.com>
2020-09-15 23:02:33 +0900

    [ADD] <handler/../test_case_..._student.go> LoginStudentAuthCase 대체 값 설정 메서드 추가

--- handler/for_test/test_case_types_for_student.go
+++ handler/for_test/test_case_types_for_student.go
@@ -11,3 +11,17 @@ type LoginStudentAuthCase struct {
 	ExpectedAccessToken			string
 	ExpectedLoggedInStudentUUID string
 }
+
+func (test *LoginStudentAuthCase) ChangeEmptyValueToValidValue() {
+	if test.StudentID == ""         { test.StudentID = validStudentID }
+	if test.StudentPW == ""         { test.StudentPW = validStudentPW }
+	if test.SpanContextString == "" { test.SpanContextString = validSpanContextString }
+	if test.XRequestID == ""        { test.XRequestID = validXRequestID }
+}
+
+func (test *LoginStudentAuthCase) ChangeEmptyReplaceValueToEmptyValue() {
+	if test.StudentID == EmptyReplaceValueForString         { test.StudentID = "" }
+	if test.StudentPW == EmptyReplaceValueForString         { test.StudentPW = "" }
+	if test.SpanContextString == EmptyReplaceValueForString { test.SpanContextString = "" }
+	if test.XRequestID == EmptyReplaceValueForString        { test.XRequestID = "" }
+}

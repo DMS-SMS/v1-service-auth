3b2c5f0ded66f5a8e2223956d7121ef14f1ede30
지농 <jinhong0719@naver.com>
2020-09-15 18:18:49 +0900

    [ADD] <handler/for_test_empty_values.go> uint32 타입 변수용 Empty Replace 변수 추가

--- handler/for_test_empty_values.go
+++ handler/for_test_empty_values.go
@@ -9,10 +9,12 @@ const (
 
 var (
 	emptyReplaceValueForString string
-	emptyReplaceValueForInt int64
+	emptyReplaceValueForInt64 int64
+	emptyReplaceValueForUint32 uint32
 )
 
 func init() {
 	emptyReplaceValueForString = random.StringConsistOfIntWithLength(10)
-	emptyReplaceValueForInt = random.Int64WithLength(10)
+	emptyReplaceValueForInt64 = random.Int64WithLength(10)
+	emptyReplaceValueForUint32 = uint32(random.Int64WithLength(10))
 }

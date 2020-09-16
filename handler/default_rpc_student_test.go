9174566242b5a3b29c5a1b3d4dfe1aedbebb5f05
지농 <jinhong0719@naver.com>
2020-09-16 22:32:12 +0900

    [ADD] <handler/default_rpc_student_test.go> ChangeStudentPW 메서드 테스트 케이스 추가

--- handler/default_rpc_student_test.go
+++ handler/default_rpc_student_test.go
@@ -101,3 +101,101 @@ func Test_default_LoginStudentAuth(t *testing.T) {
 
 	mockForDB.AssertExpectations(t)
 }
+
+func Test_default_ChangeStudentPW(t *testing.T) {
+	hashedTestPW, _ := bcrypt.GenerateFromPassword([]byte("testPW"), 1)
+
+	tests := []test.ChangeStudentPWCase{
+		{ // success case
+			UUID:        "student-111111111111",
+			StudentUUID: "student-111111111111",
+			CurrentPW:   "testPW",
+			RevisionPW:  "NewPassword",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx":                {},
+				"GetStudentAuthWithUUID": {&model.StudentAuth{StudentPW: model.StudentPW(string(hashedTestPW))}, nil},
+				"ChangeStudentPW":        {nil},
+				"Commit":                 {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusOK,
+		}, { // no exist X-Request-ID -> Proxy Authorization Required
+			XRequestID:      test.EmptyReplaceValueForString,
+			ExpectedMethods: map[test.Method]test.Returns{},
+			ExpectedStatus:  http.StatusProxyAuthRequired,
+		}, { // invalid X-Request-ID -> Proxy Authorization Required
+			XRequestID:      "InvalidXRequestID",
+			ExpectedMethods: map[test.Method]test.Returns{},
+			ExpectedStatus:  http.StatusProxyAuthRequired,
+		}, { // no exist Span-Context -> Proxy Authorization Required
+			SpanContextString: test.EmptyReplaceValueForString,
+			ExpectedMethods:   map[test.Method]test.Returns{},
+			ExpectedStatus:    http.StatusProxyAuthRequired,
+		}, { // invalid Span-Context -> Proxy Authorization Required
+			SpanContextString: "InvalidSpanContext",
+			ExpectedMethods:   map[test.Method]test.Returns{},
+			ExpectedStatus:    http.StatusProxyAuthRequired,
+		}, { // forbidden (not student)
+			UUID:           "parent-111111111112",
+			StudentUUID:    "parent-111111111112",
+			CurrentPW:      "testPW",
+			RevisionPW:     "NewPassword",
+			ExpectedStatus: http.StatusForbidden,
+		}, { // forbidden (not my auth)
+			UUID:           "student-111111111113",
+			StudentUUID:    "student-111111111114",
+			CurrentPW:      "testPW",
+			RevisionPW:     "NewPassword",
+			ExpectedStatus: http.StatusForbidden,
+		}, { // not exists student
+			UUID:           "student-111111111115",
+			StudentUUID:    "student-111111111115",
+			CurrentPW:      "testPW",
+			RevisionPW:     "NewPassword",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx":                {},
+				"GetStudentAuthWithUUID": {&model.StudentAuth{}, gorm.ErrRecordNotFound},
+				"Rollback":               {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusNotFound,
+		}, { // 현재 Password 불일치
+			UUID:        "student-111111111116",
+			StudentUUID: "student-111111111116",
+			CurrentPW:   "testPW",
+			RevisionPW:  "NewPassword",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx":                {},
+				"GetStudentAuthWithUUID": {&model.StudentAuth{StudentPW: "NotEqualPassword"}, nil},
+				"Rollback":               {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusConflict,
+			ExpectedCode:   CodeIncorrectStudentPWForChange,
+		}, { // GetStudentAuthWithUUID 에러 반환
+			UUID:        "student-111111111117",
+			StudentUUID: "student-111111111117",
+			CurrentPW:   "testPW",
+			RevisionPW:  "NewPassword",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx":                {},
+				"GetStudentAuthWithUUID": {&model.StudentAuth{}, errors.New("DB not connected")},
+				"Rollback":               {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusInternalServerError,
+		}, { // ChangeStudentPW 에러 반환
+			UUID:        "student-111111111118",
+			StudentUUID: "student-111111111118",
+			CurrentPW:   "testPW",
+			RevisionPW:  "NewPassword",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx":                {},
+				"GetStudentAuthWithUUID": {&model.StudentAuth{StudentPW: model.StudentPW(string(hashedTestPW))}, nil},
+				"ChangeStudentPW":        {errors.New("DB not connected")},
+				"Rollback":               {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusInternalServerError,
+		},
+	}
+
+	for _, testCase := range tests {
+
+	}
+}

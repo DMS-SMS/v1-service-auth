26ba86c012defe5ff20fe969e533edf6e17c6dc9
지농 <jinhong0719@naver.com>
2020-09-17 17:19:38 +0900

    [ADD] <handler/.../test_case_..._parent.go> ChangeParentPWCase 구조체 선언 및 메서드 추가

--- handler/for_test/test_case_types_for_parent.go
+++ handler/for_test/test_case_types_for_parent.go
@@ -1,6 +1,7 @@
 package test
 
 import (
+	"auth/model"
 	proto "auth/proto/golang/auth"
 	"context"
 	"github.com/micro/go-micro/v2/metadata"
@@ -56,6 +57,64 @@ func (test *LoginParentAuthCase) SetRequestContextOf(req *proto.LoginParentAuthR
 }
 
 func (test *LoginParentAuthCase) GetMetadataContext() (ctx context.Context) {
+	ctx = context.Background()
+
+	ctx = metadata.Set(ctx, "X-Request-Id", test.XRequestID)
+	ctx = metadata.Set(ctx, "Span-Context", test.SpanContextString)
+
+	return
+}
+
+type ChangeParentPWCase struct {
+	UUID, ParentUUID      string
+	CurrentPW, RevisionPW string
+	XRequestID            string
+	SpanContextString     string
+	ExpectedMethods       map[Method]Returns
+	ExpectedStatus        uint32
+	ExpectedCode          int32
+	ExpectedMessage       string
+}
+
+func (test *ChangeParentPWCase) ChangeEmptyValueToValidValue() {
+	if test.SpanContextString == "" { test.SpanContextString = validSpanContextString }
+	if test.XRequestID == ""        { test.XRequestID = validXRequestID }
+}
+
+func (test *ChangeParentPWCase) ChangeEmptyReplaceValueToEmptyValue() {
+	if test.SpanContextString == EmptyReplaceValueForString { test.SpanContextString = "" }
+	if test.XRequestID == EmptyReplaceValueForString        { test.XRequestID = "" }
+}
+
+func (test *ChangeParentPWCase) OnExpectMethods(mock *mock.Mock) {
+	for method, returns := range test.ExpectedMethods {
+		test.onMethod(mock, method, returns)
+	}
+}
+
+func (test *ChangeParentPWCase) onMethod(mock *mock.Mock, method Method, returns Returns) {
+	switch method {
+	case "BeginTx":
+		mock.On(string(method)).Return(returns...)
+	case "GetParentAuthWithUUID":
+		mock.On(string(method), test.ParentUUID).Return(returns...)
+	case "ChangeParentPW":
+		mock.On(string(method), test.ParentUUID, "").Return(returns...)
+	case "Commit":
+		mock.On(string(method)).Return(returns...)
+	case "Rollback":
+		mock.On(string(method)).Return(returns...)
+	}
+}
+
+func (test *ChangeParentPWCase) SetRequestContextOf(req *proto.ChangeParentPWRequest) {
+	req.UUID = test.UUID
+	req.ParentUUID = test.ParentUUID
+	req.CurrentPW = test.CurrentPW
+	req.RevisionPW = test.RevisionPW
+}
+
+func (test *ChangeParentPWCase) GetMetadataContext() (ctx context.Context) {
 	ctx = context.Background()
 
 	ctx = metadata.Set(ctx, "X-Request-Id", test.XRequestID)

5f86267153eca3e9ca222640c696fc2cdbfda1a3
지농 <jinhong0719@naver.com>
2020-09-17 15:00:13 +0900

    [ADD] <handler/default_rpc_teacher.go> Default LoginTeacherAuth 메서드 구현 완료

--- handler/default_rpc_teacher.go
+++ handler/default_rpc_teacher.go
@@ -1,1 +1,73 @@
 package handler
+
+import (
+	proto "auth/proto/golang/auth"
+	"context"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/log"
+	"github.com/uber/jaeger-client-go"
+	"golang.org/x/crypto/bcrypt"
+	"net/http"
+)
+
+func (h _default) LoginTeacherAuth(ctx context.Context, req *proto.LoginTeacherAuthRequest, resp *proto.LoginTeacherAuthResponse) (err error) {
+	ctx, proxyAuthenticated, reason := h.getContextFromMetadata(ctx)
+	if !proxyAuthenticated {
+		resp.Status = http.StatusProxyAuthRequired
+		resp.Message = fmt.Sprintf(proxyAuthRequiredMessageFormat, reason)
+		return
+	}
+
+	reqID := ctx.Value("X-Request-Id").(string)
+	parentSpan := ctx.Value("Span-Context").(jaeger.SpanContext)
+
+	access, err := h.accessManage.BeginTx()
+	if err != nil {
+		resp.Status = http.StatusInternalServerError
+		resp.Message = fmt.Sprintf(internalServerErrorFormat, "tx begin fail, err: " + err.Error())
+		return
+	}
+
+	spanForDB := opentracing.StartSpan("GetTeacherAuthWithID", opentracing.ChildOf(parentSpan))
+	resultAuth, err := access.GetTeacherAuthWithID(req.TeacherID)
+	spanForDB.SetTag("X-Request-Id", reqID).LogFields(log.Object("SelectedAuth", resultAuth), log.Error(err))
+	spanForDB.Finish()
+
+	if err != nil {
+		access.Rollback()
+		switch err {
+		case gorm.ErrRecordNotFound:
+			resp.Status = http.StatusConflict
+			resp.Code = CodeTeacherIDNoExist
+			resp.Message = fmt.Sprintf(conflictErrorFormat, "teacher id not exists")
+		default:
+			resp.Status = http.StatusInternalServerError
+			resp.Message = fmt.Sprintf(internalServerErrorFormat, "unable to query DB, err: " +err.Error())
+		}
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(resultAuth.TeacherPW), []byte(req.TeacherPW))
+	if err != nil {
+		access.Rollback()
+		switch err {
+		case bcrypt.ErrMismatchedHashAndPassword:
+			resp.Status = http.StatusConflict
+			resp.Code = CodeIncorrectTeacherPWForLogin
+			resp.Message = fmt.Sprintf(conflictErrorFormat, "mismatched hash and password")
+		default:
+			resp.Status = http.StatusInternalServerError
+			resp.Message = fmt.Sprintf(internalServerErrorFormat, "hash compare error, err: " + err.Error())
+		}
+		return
+	}
+
+	access.Commit()
+	resp.Status = http.StatusOK
+	resp.Message = "succeed to login teacher auth"
+	resp.LoggedInTeacherUUID = string(resultAuth.UUID)
+
+	return
+}

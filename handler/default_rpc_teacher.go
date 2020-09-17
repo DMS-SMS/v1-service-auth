50298c9a507e26bceddcc2b63f33ee27e2912f4c
지농 <jinhong0719@naver.com>
2020-09-17 15:53:44 +0900

    [ADD] <handler/default_rpc_teacher.go> Default ChangeTeacherPW 메서드 추가 및 구현 완료

--- handler/default_rpc_teacher.go
+++ handler/default_rpc_teacher.go
@@ -71,3 +71,84 @@ func (h _default) LoginTeacherAuth(ctx context.Context, req *proto.LoginTeacherA
 
 	return
 }
+
+func (h _default) ChangeTeacherPW(ctx context.Context, req *proto.ChangeTeacherPWRequest, resp *proto.ChangeTeacherPWResponse) (err error) {
+	ctx, proxyAuthenticated, reason := h.getContextFromMetadata(ctx)
+	if !proxyAuthenticated {
+		resp.Status = http.StatusProxyAuthRequired
+		resp.Message = fmt.Sprintf(proxyAuthRequiredMessageFormat, reason)
+		return
+	}
+
+	if !teacherUUIDRegex.MatchString(req.TeacherUUID) {
+		resp.Status = http.StatusForbidden
+		resp.Message = fmt.Sprintf(forbiddenMessageFormat, "this API is for teachers only")
+		return
+	}
+
+	if req.UUID != req.TeacherUUID {
+		resp.Status = http.StatusForbidden
+		resp.Message = fmt.Sprintf(forbiddenMessageFormat, "not your auth, uuid: " + req.TeacherUUID)
+		return
+	}
+
+	reqID := ctx.Value("X-Request-Id").(string)
+	parentSpan := ctx.Value("Span-Context").(jaeger.SpanContext)
+
+	access, err := h.accessManage.BeginTx()
+	if err != nil {
+		resp.Status = http.StatusInternalServerError
+		resp.Message = fmt.Sprintf(internalServerErrorFormat, "tx begin fail, err: " + err.Error())
+		return
+	}
+
+	spanForDB := opentracing.StartSpan("GetTeacherAuthWithUUID", opentracing.ChildOf(parentSpan))
+	selectedAuth, err := access.GetTeacherAuthWithUUID(req.TeacherUUID)
+	spanForDB.SetTag("X-Request-Id", reqID).LogFields(log.Object("SelectedAuth", selectedAuth), log.Error(err))
+	spanForDB.Finish()
+
+	if err != nil {
+		access.Rollback()
+		switch err {
+		case gorm.ErrRecordNotFound:
+			resp.Status = http.StatusNotFound
+			resp.Message = fmt.Sprintf(notFoundMessageFormat, "not exist teacher, uuid: " + req.TeacherUUID)
+		default:
+			resp.Status = http.StatusInternalServerError
+			resp.Message = fmt.Sprintf(internalServerErrorFormat, "unable to query DB, err: " + err.Error())
+		}
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(selectedAuth.TeacherPW), []byte(req.CurrentPW))
+	if err != nil {
+		access.Rollback()
+		switch err {
+		case bcrypt.ErrMismatchedHashAndPassword:
+			resp.Status = http.StatusConflict
+			resp.Code = CodeIncorrectTeacherPWForChange
+			resp.Message = fmt.Sprintf(conflictErrorFormat, "mismatched hash and password")
+		default:
+			resp.Status = http.StatusInternalServerError
+			resp.Message = fmt.Sprintf(internalServerErrorFormat, "hash compare error, err: " + err.Error())
+		}
+		return
+	}
+
+	spanForDB = opentracing.StartSpan("ChangeTeacherPW", opentracing.ChildOf(parentSpan))
+	err = access.ChangeTeacherPW(string(selectedAuth.UUID), req.RevisionPW)
+	spanForDB.SetTag("X-Request-Id", reqID).LogFields(log.Error(err))
+	spanForDB.Finish()
+
+	if err != nil {
+		access.Rollback()
+		resp.Status = http.StatusInternalServerError
+		resp.Message = fmt.Sprintf(internalServerErrorFormat, "unable to update DB, err: " + err.Error())
+		return
+	}
+
+	access.Commit()
+	resp.Status = http.StatusOK
+	resp.Message = "teacher pw change success"
+	return
+}

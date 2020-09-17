f34bb5830abaf21428f0ef199c6703024e044291
지농 <jinhong0719@naver.com>
2020-09-17 18:45:50 +0900

    [ADD] <handler/default_rpc_parent.go> Default LoginParentAuth 선언 추가 및 구현 완료

--- handler/default_rpc_parent.go
+++ handler/default_rpc_parent.go
@@ -1,1 +1,73 @@
 package handler
+
+import (
+	proto "auth/proto/golang/auth"
+	"context"
+	"fmt"
+	"github.com/jinzhu/gorm"
+	"github.com/opentracing/opentracing-go"
+	"github.com/opentracing/opentracing-go/log"
+	"github.com/uber/jaeger-client-go"
+	"golang.org/x/crypto/bcrypt"
+	"net/http"
+)
+
+func (h _default) LoginParentAuth(ctx context.Context, req *proto.LoginParentAuthRequest, resp *proto.LoginParentAuthResponse) (err error) {
+	ctx, proxyAuthenticated, reason := h.getContextFromMetadata(ctx)
+	if !proxyAuthenticated {
+		resp.Status = http.StatusProxyAuthRequired
+		resp.Message = fmt.Sprintf(proxyAuthRequiredMessageFormat, reason)
+		return
+	}
+
+	reqID := ctx.Value("X-Request-Id").(string)
+	parentSpan := ctx.Value("Span-Context").(jaeger.SpanContext)
+
+	access, err := h.accessManage.BeginTx()
+	if err != nil {
+		resp.Status = http.StatusInternalServerError
+		resp.Message = fmt.Sprintf(internalServerErrorFormat, "tx begin fail, err: " + err.Error())
+		return
+	}
+
+	spanForDB := opentracing.StartSpan("GetParentAuthWithID", opentracing.ChildOf(parentSpan))
+	resultAuth, err := access.GetParentAuthWithID(req.ParentID)
+	spanForDB.SetTag("X-Request-Id", reqID).LogFields(log.Object("SelectedAuth", resultAuth), log.Error(err))
+	spanForDB.Finish()
+
+	if err != nil {
+		access.Rollback()
+		switch err {
+		case gorm.ErrRecordNotFound:
+			resp.Status = http.StatusConflict
+			resp.Code = CodeParentIDNoExist
+			resp.Message = fmt.Sprintf(conflictErrorFormat, "parent id not exists")
+		default:
+			resp.Status = http.StatusInternalServerError
+			resp.Message = fmt.Sprintf(internalServerErrorFormat, "unable to query DB, err: " +err.Error())
+		}
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(resultAuth.ParentPW), []byte(req.ParentPW))
+	if err != nil {
+		access.Rollback()
+		switch err {
+		case bcrypt.ErrMismatchedHashAndPassword:
+			resp.Status = http.StatusConflict
+			resp.Code = CodeIncorrectParentPWForLogin
+			resp.Message = fmt.Sprintf(conflictErrorFormat, "mismatched hash and password")
+		default:
+			resp.Status = http.StatusInternalServerError
+			resp.Message = fmt.Sprintf(internalServerErrorFormat, "hash compare error, err: " + err.Error())
+		}
+		return
+	}
+
+	access.Commit()
+	resp.Status = http.StatusOK
+	resp.Message = "succeed to login parent auth"
+	resp.LoggedInParentUUID = string(resultAuth.UUID)
+
+	return
+}

9f36abd9ec52e45c7b33f83bf1adf8f38da4c161
지농 <jinhong0719@naver.com>
2020-09-17 16:43:43 +0900

    [ADD] <handler/default_rpc_teacher.go> GetTeacherInformWithUUID 메서드 구현 완료

--- handler/default_rpc_teacher.go
+++ handler/default_rpc_teacher.go
@@ -152,3 +152,62 @@ func (h _default) ChangeTeacherPW(ctx context.Context, req *proto.ChangeTeacherP
 	resp.Message = "teacher pw change success"
 	return
 }
+
+func (h _default) GetTeacherInformWithUUID(ctx context.Context, req *proto.GetTeacherInformWithUUIDRequest, resp *proto.GetTeacherInformWithUUIDResponse) (err error) {
+	ctx, proxyAuthenticated, reason := h.getContextFromMetadata(ctx)
+	if !proxyAuthenticated {
+		resp.Status = http.StatusProxyAuthRequired
+		resp.Message = fmt.Sprintf(proxyAuthRequiredMessageFormat, reason)
+		return
+	}
+
+	if !teacherUUIDRegex.MatchString(req.UUID) {
+		resp.Status = http.StatusForbidden
+		resp.Message = fmt.Sprintf(forbiddenMessageFormat, "this API is for teachers only")
+		return
+	}
+
+	if req.UUID != req.TeacherUUID {
+		resp.Status = http.StatusForbidden
+		resp.Message = fmt.Sprintf(forbiddenMessageFormat, "not your auth, uuid: " + req.TeacherUUID)
+		return
+	}
+
+	reqID := ctx.Value("X-Request-Id").(string)
+	parentSpan := ctx.Value("Span-Context").(jaeger.SpanContext)
+
+	access, err := h.accessManage.BeginTx()
+	if err != nil {
+		resp.Status = http.StatusInternalServerError
+		resp.Message = fmt.Sprintf(internalServerErrorFormat, "tx begin fail, err: " + err.Error())
+		return
+	}
+
+	spanForDB := opentracing.StartSpan("GetTeacherInformWithUUID", opentracing.ChildOf(parentSpan))
+	selectedAuth, err := access.GetTeacherInformWithUUID(req.TeacherUUID)
+	spanForDB.SetTag("X-Request-Id", reqID).LogFields(log.Object("SelectedAuth", selectedAuth), log.Error(err))
+	spanForDB.Finish()
+
+	if err != nil {
+		access.Rollback()
+		switch err {
+		case gorm.ErrRecordNotFound:
+			resp.Status = http.StatusNotFound
+			resp.Message = fmt.Sprintf(notFoundMessageFormat, "not exist teacher, uuid: " + req.TeacherUUID)
+		default:
+			resp.Status = http.StatusInternalServerError
+			resp.Message = fmt.Sprintf(internalServerErrorFormat, "unable to query DB, err: " + err.Error())
+		}
+		return
+	}
+
+	access.Commit()
+	resp.Grade = uint32(selectedAuth.Grade)
+	resp.Class = uint32(selectedAuth.Class)
+	resp.Name = string(selectedAuth.Name)
+	resp.PhoneNumber = string(selectedAuth.PhoneNumber)
+
+	resp.Status = http.StatusOK
+	resp.Message = "get teacher auth success"
+	return
+}

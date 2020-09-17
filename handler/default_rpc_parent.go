4cada24f6eeccf1260511dc4654ff266703e54e7
지농 <jinhong0719@naver.com>
2020-09-17 18:50:15 +0900

    [ADD] <handler/default_rpc_parent.go> GetParentInformWithUUID 메서드 선언 및 구현 완료

--- handler/default_rpc_parent.go
+++ handler/default_rpc_parent.go
@@ -152,3 +152,60 @@ func (h _default) ChangeParentPW(ctx context.Context, req *proto.ChangeParentPWR
 	resp.Message = "parent pw change success"
 	return
 }
+
+func (h _default) GetParentInformWithUUID(ctx context.Context, req *proto.GetParentInformWithUUIDRequest, resp *proto.GetParentInformWithUUIDResponse) (err error) {
+	ctx, proxyAuthenticated, reason := h.getContextFromMetadata(ctx)
+	if !proxyAuthenticated {
+		resp.Status = http.StatusProxyAuthRequired
+		resp.Message = fmt.Sprintf(proxyAuthRequiredMessageFormat, reason)
+		return
+	}
+
+	if !parentUUIDRegex.MatchString(req.UUID) {
+		resp.Status = http.StatusForbidden
+		resp.Message = fmt.Sprintf(forbiddenMessageFormat, "this API is for parents only")
+		return
+	}
+
+	if req.UUID != req.ParentUUID {
+		resp.Status = http.StatusForbidden
+		resp.Message = fmt.Sprintf(forbiddenMessageFormat, "not your auth, uuid: " + req.ParentUUID)
+		return
+	}
+
+	reqID := ctx.Value("X-Request-Id").(string)
+	parentSpan := ctx.Value("Span-Context").(jaeger.SpanContext)
+
+	access, err := h.accessManage.BeginTx()
+	if err != nil {
+		resp.Status = http.StatusInternalServerError
+		resp.Message = fmt.Sprintf(internalServerErrorFormat, "tx begin fail, err: " + err.Error())
+		return
+	}
+
+	spanForDB := opentracing.StartSpan("GetParentInformWithUUID", opentracing.ChildOf(parentSpan))
+	selectedAuth, err := access.GetParentInformWithUUID(req.ParentUUID)
+	spanForDB.SetTag("X-Request-Id", reqID).LogFields(log.Object("SelectedAuth", selectedAuth), log.Error(err))
+	spanForDB.Finish()
+
+	if err != nil {
+		access.Rollback()
+		switch err {
+		case gorm.ErrRecordNotFound:
+			resp.Status = http.StatusNotFound
+			resp.Message = fmt.Sprintf(notFoundMessageFormat, "not exist parent, uuid: " + req.ParentUUID)
+		default:
+			resp.Status = http.StatusInternalServerError
+			resp.Message = fmt.Sprintf(internalServerErrorFormat, "unable to query DB, err: " + err.Error())
+		}
+		return
+	}
+
+	access.Commit()
+	resp.Name = string(selectedAuth.Name)
+	resp.PhoneNumber = string(selectedAuth.PhoneNumber)
+
+	resp.Status = http.StatusOK
+	resp.Message = "get parent auth success"
+	return
+}

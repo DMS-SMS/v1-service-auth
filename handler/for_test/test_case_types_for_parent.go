11a9bce31d3e4d9580319981420ba4ddd4f6b311
지농 <jinhong0719@naver.com>
2020-09-17 17:21:03 +0900

    [ADD] <handler/.../..._parent.go> GetParentInformWithUUIDCase 구조체 선언 및 메서드 추가

--- handler/for_test/test_case_types_for_parent.go
+++ handler/for_test/test_case_types_for_parent.go
@@ -122,3 +122,57 @@ func (test *ChangeParentPWCase) GetMetadataContext() (ctx context.Context) {
 
 	return
 }
+
+type GetParentInformWithUUIDCase struct {
+	UUID, ParentUUID  string
+	XRequestID        string
+	SpanContextString string
+	ExpectedMethods   map[Method]Returns
+	ExpectedStatus    uint32
+	ExpectedCode      int32
+	ExpectedMessage   string
+	ExpectedInform    *model.ParentInform
+}
+
+func (test *GetParentInformWithUUIDCase) ChangeEmptyValueToValidValue() {
+	if test.XRequestID == ""        { test.XRequestID = validXRequestID }
+	if test.SpanContextString == "" { test.SpanContextString = validSpanContextString }
+}
+
+func (test *GetParentInformWithUUIDCase) ChangeEmptyReplaceValueToEmptyValue() {
+	if test.XRequestID == EmptyReplaceValueForString        { test.XRequestID = "" }
+	if test.SpanContextString == EmptyReplaceValueForString { test.SpanContextString = "" }
+}
+
+func (test *GetParentInformWithUUIDCase) OnExpectMethods(mock *mock.Mock) {
+	for method, returns := range test.ExpectedMethods {
+		test.onMethod(mock, method, returns)
+	}
+}
+
+func (test *GetParentInformWithUUIDCase) onMethod(mock *mock.Mock, method Method, returns Returns) {
+	switch method {
+	case "BeginTx":
+		mock.On(string(method)).Return(returns...)
+	case "GetParentInformWithUUID":
+		mock.On(string(method), test.ParentUUID).Return(returns...)
+	case "Commit":
+		mock.On(string(method)).Return(returns...)
+	case "Rollback":
+		mock.On(string(method)).Return(returns...)
+	}
+}
+
+func (test *GetParentInformWithUUIDCase) SetRequestContextOf(req *proto.GetParentInformWithUUIDRequest) {
+	req.UUID = test.UUID
+	req.ParentUUID = test.ParentUUID
+}
+
+func (test *GetParentInformWithUUIDCase) GetMetadataContext() (ctx context.Context) {
+	ctx = context.Background()
+
+	ctx = metadata.Set(ctx, "X-Request-Id", test.XRequestID)
+	ctx = metadata.Set(ctx, "Span-Context", test.SpanContextString)
+
+	return
+}

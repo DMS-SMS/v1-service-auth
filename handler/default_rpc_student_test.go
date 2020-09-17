d9b24c31108ca476cda44cbd8993a8eae202e65e
지농 <jinhong0719@naver.com>
2020-09-17 13:24:16 +0900

    [ADD] <handler/default_rpc_student_test.go> GetStudentInformWithUUID 테스트 케이스 추가

--- handler/default_rpc_student_test.go
+++ handler/default_rpc_student_test.go
@@ -10,6 +10,7 @@ import (
 	"golang.org/x/crypto/bcrypt"
 	"net/http"
 	"testing"
+	"time"
 )
 
 func Test_default_LoginStudentAuth(t *testing.T) {
@@ -234,5 +235,83 @@ func Test_default_ChangeStudentPW(t *testing.T) {
 
 		assert.Equalf(t, int(testCase.ExpectedStatus), int(resp.Status), "status assertion error (test case: %v, message: %s)", testCase, resp.Message)
 		assert.Equalf(t, int(testCase.ExpectedCode), int(resp.Code), "code assertion error (test case: %v, message: %s)", testCase, resp.Message)
+	}
+}
+
+func Test_default_GetStudentInformWithUUID(t *testing.T) {
+	now := time.Now()
+
+	tests := []test.GetStudentInformWithUUIDCase{
+		{ // success case
+			UUID: "student-111111111111",
+			StudentUUID: "student-111111111111",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx": {},
+				"GetStudentInformWithUUID": {&model.StudentInform{
+					Model:         gorm.Model{CreatedAt: now, UpdatedAt: now},
+					StudentUUID:   "student-111111111111",
+					Grade:         2,
+					Class:         2,
+					StudentNumber: 7,
+					Name:          "박진홍",
+					PhoneNumber:   "01088378347",
+					ProfileURI:    "/profiles/student-111111111111",
+				}, nil},
+				"Commit": {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusOK,
+			ExpectedInform: &model.StudentInform{
+				Model:         gorm.Model{CreatedAt: now, UpdatedAt: now},
+				StudentUUID:   "student-111111111111",
+				Grade:         2,
+				Class:         2,
+				StudentNumber: 7,
+				Name:          "박진홍",
+				PhoneNumber:   "01088378347",
+				ProfileURI:    "/profiles/student-111111111111",
+			},
+		}, { // no exist X-Request-ID -> Proxy Authorization Required
+			XRequestID:      test.EmptyReplaceValueForString,
+			ExpectedMethods: map[test.Method]test.Returns{},
+			ExpectedStatus:  http.StatusProxyAuthRequired,
+		}, { // invalid X-Request-ID -> Proxy Authorization Required
+			XRequestID:      "InvalidXRequestID",
+			ExpectedMethods: map[test.Method]test.Returns{},
+			ExpectedStatus:  http.StatusProxyAuthRequired,
+		}, { // no exist Span-Context -> Proxy Authorization Required
+			SpanContextString: test.EmptyReplaceValueForString,
+			ExpectedMethods:   map[test.Method]test.Returns{},
+			ExpectedStatus:    http.StatusProxyAuthRequired,
+		}, { // invalid Span-Context -> Proxy Authorization Required
+			SpanContextString: "InvalidSpanContext",
+			ExpectedMethods:   map[test.Method]test.Returns{},
+			ExpectedStatus:    http.StatusProxyAuthRequired,
+		}, { // forbidden (not student)
+			UUID:           "parent-111111111112",
+			StudentUUID:    "student-111111111112",
+			ExpectedStatus: http.StatusForbidden,
+		}, { // forbidden (not my auth)
+			UUID:           "student-111111111113",
+			StudentUUID:    "student-111111111114",
+			ExpectedStatus: http.StatusForbidden,
+		}, { // no exist student uuid
+			UUID:        "student-111111111115",
+			StudentUUID: "student-111111111115",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx":                  {},
+				"GetStudentInformWithUUID": {&model.StudentInform{}, gorm.ErrRecordNotFound},
+				"Rollback":                 {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusNotFound,
+		}, { // GetStudentInformWithUUID error return
+			UUID:        "student-111111111116",
+			StudentUUID: "student-111111111116",
+			ExpectedMethods: map[test.Method]test.Returns{
+				"BeginTx":                  {},
+				"GetStudentInformWithUUID": {&model.StudentInform{}, errors.New("DB not connected")},
+				"Rollback":                 {&gorm.DB{}},
+			},
+			ExpectedStatus: http.StatusInternalServerError,
+		},
 	}
 }

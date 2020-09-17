e881cca78014da8d75ac85b4936fe84cb3b4a6a8
지농 <jinhong0719@naver.com>
2020-09-17 14:19:54 +0900

    [ADD] <handler/.../test_case_..._teacher.go> LoginTeacherAuth 테스트 주도 객체 선언

--- handler/for_test/test_case_types_for_teacher.go
+++ handler/for_test/test_case_types_for_teacher.go
@@ -1,1 +1,13 @@
 package test
+
+type LoginTeacherAuthCase struct {
+	TeacherID, TeacherPW        string
+	XRequestID                  string
+	SpanContextString           string
+	ExpectedMethods             map[Method]Returns
+	ExpectedStatus              uint32
+	ExpectedCode                int32
+	ExpectedMessage             string
+	ExpectedAccessToken			string
+	ExpectedLoggedInTeacherUUID string
+}

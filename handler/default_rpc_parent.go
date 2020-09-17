73a5ccba9cdce603dfb485ab617d409a9249c161
지농 <jinhong0719@naver.com>
2020-09-17 18:48:28 +0900

    [ADD] <handler/default_rpc_parent.go> Default ChangeParentPW 메서드 추가 및 구현 완료

--- handler/default_rpc_parent.go
+++ handler/default_rpc_parent.go
@@ -71,3 +71,84 @@ func (h _default) LoginParentAuth(ctx context.Context, req *proto.LoginParentAut
 
 	return
 }
+
+func (h _default) ChangeParentPW(ctx context.Context, req *proto.ChangeParentPWRequest, resp *proto.ChangeParentPWResponse) (err error) {
+	ctx, proxyAuthenticated, reason := h.getContextFromMetadata(ctx)
+	if !proxyAuthenticated {
+		resp.Status = http.StatusProxyAuthRequired
+		resp.Message = fmt.Sprintf(proxyAuthRequiredMessageFormat, reason)
+		return
+	}
+
+	if !parentUUIDRegex.MatchString(req.ParentUUID) {
+		resp.Status = http.StatusForbidden
+		resp.Message = fmt.Sprintf(forbiddenMessageFormat, "this API is for parents only")
+		return
+	}
+
+	if req.UUID != req.ParentUUID {
+		resp.Status = http.StatusForbidden
+		resp.Message = fmt.Sprintf(forbiddenMessageFormat, "not your auth, uuid: " + req.ParentUUID)
+		return
+	}
+
+	reqID := ctx.Value("X-Request-Id").(string)
+	parentSpan := ctx.Value("Span-Context").(jaeger.SpanContext)
+
+	access, err := h.accessManage.BeginTx()
+	if err != nil {
+		resp.Status = http.StatusInternalServerError
+		resp.Message = fmt.Sprintf(internalServerErrorFormat, "tx begin fail, err: " + err.Error())
+		return
+	}
+
+	spanForDB := opentracing.StartSpan("GetParentAuthWithUUID", opentracing.ChildOf(parentSpan))
+	selectedAuth, err := access.GetParentAuthWithUUID(req.ParentUUID)
+	spanForDB.SetTag("X-Request-Id", reqID).LogFields(log.Object("SelectedAuth", selectedAuth), log.Error(err))
+	spanForDB.Finish()
+
+	if err != nil {
+		access.Rollback()
+		switch err {
+		case gorm.ErrRecordNotFound:
+			resp.Status = http.StatusNotFound
+			resp.Message = fmt.Sprintf(notFoundMessageFormat, "not exist parent, uuid: " + req.ParentUUID)
+		default:
+			resp.Status = http.StatusInternalServerError
+			resp.Message = fmt.Sprintf(internalServerErrorFormat, "unable to query DB, err: " + err.Error())
+		}
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(selectedAuth.ParentPW), []byte(req.CurrentPW))
+	if err != nil {
+		access.Rollback()
+		switch err {
+		case bcrypt.ErrMismatchedHashAndPassword:
+			resp.Status = http.StatusConflict
+			resp.Code = CodeIncorrectParentPWForChange
+			resp.Message = fmt.Sprintf(conflictErrorFormat, "mismatched hash and password")
+		default:
+			resp.Status = http.StatusInternalServerError
+			resp.Message = fmt.Sprintf(internalServerErrorFormat, "hash compare error, err: " + err.Error())
+		}
+		return
+	}
+
+	spanForDB = opentracing.StartSpan("ChangeParentPW", opentracing.ChildOf(parentSpan))
+	err = access.ChangeParentPW(string(selectedAuth.UUID), req.RevisionPW)
+	spanForDB.SetTag("X-Request-Id", reqID).LogFields(log.Error(err))
+	spanForDB.Finish()
+
+	if err != nil {
+		access.Rollback()
+		resp.Status = http.StatusInternalServerError
+		resp.Message = fmt.Sprintf(internalServerErrorFormat, "unable to update DB, err: " + err.Error())
+		return
+	}
+
+	access.Commit()
+	resp.Status = http.StatusOK
+	resp.Message = "parent pw change success"
+	return
+}

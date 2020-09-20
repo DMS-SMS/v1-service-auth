3f52cd0771f6b394a6ae9cddaa92584bac789db4
지농 <jinhong0719@naver.com>
2020-09-20 21:26:15 +0900

    [ADD] <tool/closure/consul.go> Port 및 Addr 파싱 및 조회 관련 내부 함수 선언 추가

--- tool/closure/consul.go
+++ tool/closure/consul.go
@@ -1,1 +1,34 @@
 package closure
+
+import (
+	"errors"
+	"github.com/micro/go-micro/v2/server"
+	"net"
+	"strconv"
+	"strings"
+)
+
+func getPortFromServerOption(opts server.Options) (port int, err error) {
+	const portIndex = 3
+	portStr := strings.Split(opts.Address, ":")[portIndex]
+	port, err = strconv.Atoi(portStr)
+	return
+}
+
+func getMyLocalAddr() (addr *net.UDPAddr, err error) {
+	conn, err := net.Dial("udp", "8.8.8.8:80")
+	if err != nil {
+		return
+	}
+	defer func() {
+		_ = conn.Close()
+	}()
+
+	addr, ok := conn.LocalAddr().(*net.UDPAddr)
+	if ok {
+		err = errors.New("unable to assert type to *net.UDPAddr")
+		return
+	}
+
+	return
+}

9eb84801c6f157d480e1e2c34a5d412790f722d8
지농 <jinhong0719@naver.com>
2020-09-21 20:09:45 +0900

    [ADD] <handler/none.go> GetXXXUUIDsWithInform 메서드 추가에 따른 None 객체 수정

--- handler/none.go
+++ handler/none.go
@@ -17,13 +17,16 @@ func(n None) LoginAdminAuth(context.Context, *proto.LoginAdminAuthRequest, *prot
 func(n None) LoginStudentAuth(context.Context, *proto.LoginStudentAuthRequest, *proto.LoginStudentAuthResponse) (err error) { return }
 func(n None) ChangeStudentPW(context.Context, *proto.ChangeStudentPWRequest, *proto.ChangeStudentPWResponse) (err error) { return }
 func(n None) GetStudentInformWithUUID(context.Context, *proto.GetStudentInformWithUUIDRequest, *proto.GetStudentInformWithUUIDResponse) (err error) { return }
+func(n None) GetStudentUUIDsWithInform(context.Context, *proto.GetStudentUUIDsWithInformRequest, *proto.GetStudentUUIDsWithInformResponse) (err error) { return }
 
 // About Teacher RPC Service
 func(n None) LoginTeacherAuth(context.Context, *proto.LoginTeacherAuthRequest, *proto.LoginTeacherAuthResponse) (err error) { return }
 func(n None) ChangeTeacherPW(context.Context, *proto.ChangeTeacherPWRequest, *proto.ChangeTeacherPWResponse) (err error) { return }
 func(n None) GetTeacherInformWithUUID(context.Context, *proto.GetTeacherInformWithUUIDRequest, *proto.GetTeacherInformWithUUIDResponse) (err error) { return }
+func(n None) GetTeacherUUIDsWithInform(context.Context, *proto.GetTeacherUUIDsWithInformRequest, *proto.GetTeacherUUIDsWithInformResponse) (err error) { return }
 
 // About Parent RPC Service
 func(n None) LoginParentAuth(context.Context, *proto.LoginParentAuthRequest, *proto.LoginParentAuthResponse) (err error) { return }
 func(n None) ChangeParentPW(context.Context, *proto.ChangeParentPWRequest, *proto.ChangeParentPWResponse) (err error) { return }
 func(n None) GetParentInformWithUUID(context.Context, *proto.GetParentInformWithUUIDRequest, *proto.GetParentInformWithUUIDResponse) (err error) { return }
+func(n None) GetParentUUIDsWithInform(context.Context, *proto.GetParentUUIDsWithInformRequest, *proto.GetParentUUIDsWithInformResponse) (err error) { return }

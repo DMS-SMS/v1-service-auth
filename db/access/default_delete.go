c00c796ee6a9e90b3ddd9823f0fb9111484c7193
지농 <jinhong0719@naver.com>
2020-10-05 22:45:47 +0900

    [ADD] <db/access/default_delete.go> DeleteXXXInform 메서드 추가 구현 완료

--- db/access/default_delete.go
+++ db/access/default_delete.go
@@ -18,3 +18,18 @@ func (d *_default) DeleteParentAuth(uuid string) (err error) {
 	err = d.tx.Where("uuid = ?", uuid).Delete(&model.ParentAuth{}).Error
 	return
 }
+
+func (d *_default) DeleteStudentInform(studentUUID string) (err error) {
+	err = d.tx.Where("student_uuid = ?", studentUUID).Delete(&model.StudentInform{}).Error
+	return
+}
+
+func (d *_default) DeleteTeacherInform(teacherUUID string) (err error) {
+	err = d.tx.Where("teacher_uuid = ?", teacherUUID).Delete(&model.TeacherInform{}).Error
+	return
+}
+
+func (d *_default) DeleteParentInform(parentUUID string) (err error) {
+	err = d.tx.Where("parent_uuid = ?", parentUUID).Delete(&model.ParentInform{}).Error
+	return
+}

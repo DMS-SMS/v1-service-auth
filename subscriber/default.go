90e1db85611cdd70f57adafa6ad779503fda71e7
지농 <jinhong0719@naver.com>
2021-01-20 17:29:27 +0900

    [CREATE] <subscriber/default.go> Create file to declare event subscriber

---
+++ subscriber/default.go
@@ -0,0 +1,5 @@
+// Add package in v.1.1.6
+// subscriber package is used for handling event message occurred by SNS, RabbitMQ, etc ...
+// you can start subscribe by calling Start method with parameter, specific signature function
+
+package subscriber

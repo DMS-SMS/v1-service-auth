241bd998d30b5da39b8c7d273edf6c493f4a3a47
지농 <jinhong0719@naver.com>
2021-01-20 17:38:54 +0900

    [ADD] <subscribe/default.go> Add default constructor & awsSession FieldSetter

--- subscriber/default.go
+++ subscriber/default.go
@@ -11,3 +11,23 @@ import (
 type _default struct {
 	awsSession *session.Session
 }
+
+type FieldSetter func(*_default)
+
+func Default(setters ...FieldSetter) *_default {
+	return newDefault(setters...)
+}
+
+func newDefault(setters ...FieldSetter) (h *_default) {
+	h = new(_default)
+	for _, setter := range setters {
+		setter(h)
+	}
+	return
+}
+
+func AWSSession(awsSession *session.Session) FieldSetter {
+	return func(s *_default) {
+		s.awsSession = awsSession
+	}
+}

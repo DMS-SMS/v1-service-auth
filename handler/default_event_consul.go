b8cff3df8aa96835d1b07047ff8f0c00f5b275fe
지농 <jinhong0719@naver.com>
2021-01-20 18:17:34 +0900

    [ADD] <handler/default_event_consul.go> Add ChangeConsulNodes method

--- handler/default_event_consul.go
+++ handler/default_event_consul.go
@@ -2,3 +2,9 @@
 // this file declare method that handling event about consul in _default struct
 
 package handler
+
+import "github.com/aws/aws-sdk-go/service/sqs"
+
+func (h *_default) ChangeConsulNodes(output *sqs.ReceiveMessageOutput) {
+
+}

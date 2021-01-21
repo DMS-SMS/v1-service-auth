1522aafb598c4e0da1c96589c453f83641ae5072
지농 <jinhong0719@naver.com>
2021-01-21 11:36:42 +0900

    [MODIFY] <tool/consul/agent/default.go> Move GetNextServiceNode to default_method.go

--- tool/consul/agent/default.go
+++ tool/consul/agent/default.go
@@ -4,12 +4,9 @@
 package agent
 
 import (
-	"errors"
-	"fmt"
 	"github.com/hashicorp/consul/api"
 	"github.com/micro/go-micro/v2/client/selector"
 	"github.com/micro/go-micro/v2/registry"
-	"reflect"
 	"sync"
 )
 
@@ -51,40 +48,3 @@ func Strategy(s selector.Strategy) FieldSetter {
 		d.Strategy = s
 	}
 }
-
-const StatusMustBePassing = "Status==passing"
-
-func (d *_default) GetNextServiceNode(service string) (*registry.Node, error) {
-	checks, _, err := d.client.Health().Checks(service, &api.QueryOptions{Filter: StatusMustBePassing})
-	if err != nil {
-		return nil, errors.New(fmt.Sprintf("unable to query health checkes, err: %v", err))
-	}
-
-	if len(checks) == 0 {
-		return nil, ErrAvailableNodeNotFound
-	}
-
-	var nodes []*registry.Node
-	for _, check := range checks {
-		as, _, err := d.client.Agent().Service(check.ServiceID, nil)
-		if err != nil {
-			return nil, errors.New(fmt.Sprintf("unable to query service, err: %v", err))
-		}
-		var md = map[string]string{"CheckID": check.CheckID}
-		node := &registry.Node{Id: as.ID, Address: fmt.Sprintf("%s:%d", as.Address, as.Port), Metadata: md}
-		nodes = append(nodes, node)
-	}
-
-	if !reflect.DeepEqual(d.nodes, nodes) {
-		d.nodes = nodes
-		d.next = d.Strategy([]*registry.Service{{Nodes: nodes}})
-	}
-
-	selectedNode, err := d.next()
-	if err != nil {
-		return nil, errors.New(fmt.Sprintf("unable to select node in selector, err: %v", err))
-	}
-
-	return selectedNode, nil
-}
-

a8d10acd8723790d50ac137214c35a4eebe1311c
지농 <jinhong0719@naver.com>
2021-01-21 10:06:55 +0900

    [CREATE] <tool/consul/agent/error.go> Create file clone from club repository

---
+++ tool/consul/agent/mock.go
@@ -0,0 +1,23 @@
+// add package in v.1.1.6
+// clone from tool/consul/agent in club
+
+package agent
+
+import (
+	"github.com/micro/go-micro/v2/registry"
+	"github.com/stretchr/testify/mock"
+)
+
+type _mock struct {
+	mock *mock.Mock
+}
+
+func Mock(mock *mock.Mock) _mock {
+	return _mock{mock: mock}
+}
+
+
+func (m _mock) GetNextServiceNode(service string) (*registry.Node, error) {
+	args := m.mock.Called(service)
+	return args.Get(0).(*registry.Node), args.Error(1)
+}

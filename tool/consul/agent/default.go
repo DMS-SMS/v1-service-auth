27f0564f00d969a141362d97022aa738c940b1e4
지농 <jinhong0719@naver.com>
2021-01-21 12:17:20 +0900

    [MODIFY] <tool/consul/agent/default.go> Modify as chaning package of ServiceName

--- tool/consul/agent/default.go
+++ tool/consul/agent/default.go
@@ -4,6 +4,7 @@
 package agent
 
 import (
+	"auth/tool/consul"
 	"github.com/hashicorp/consul/api"
 	"github.com/micro/go-micro/v2/client/selector"
 	"github.com/micro/go-micro/v2/registry"
@@ -15,9 +16,9 @@ type _default struct {
 	client    *api.Client
 //  next      selector.Next                    // before v.1.1.6
 //  nodes     []*registry.Node                 // before v.1.1.6
-	next      map[ServiceName]selector.Next    // change in v.1.1.6
-	nodes     map[ServiceName][]*registry.Node // change in v.1.1.6
-	services  []ServiceName                    // add in v.1.1.6
+	next      map[consul.ServiceName]selector.Next    // change in v.1.1.6
+	nodes     map[consul.ServiceName][]*registry.Node // change in v.1.1.6
+	services  []consul.ServiceName                    // add in v.1.1.6
 	nodeMutex sync.RWMutex                     // add in v.1.1.6
 }
 
@@ -30,7 +31,7 @@ func newDefault(setters ...FieldSetter) (h *_default) {
 	for _, setter := range setters {
 		setter(h)
 	}
-	h.nodes = map[ServiceName][]*registry.Node{}
+	h.nodes = map[consul.ServiceName][]*registry.Node{}
 	h.nodeMutex = sync.RWMutex{}
 	return
 }
@@ -49,7 +50,7 @@ func Strategy(s selector.Strategy) FieldSetter {
 	}
 }
 
-func Services(s []ServiceName) FieldSetter {
+func Services(s []consul.ServiceName) FieldSetter {
 	return func(d *_default) {
 		d.services = s
 	}

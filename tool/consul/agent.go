f76a2835aaafa09d8701ad025ab4d53075e216d6
지농 <jinhong0719@naver.com>
2021-01-21 10:57:43 +0900

    [ADD] <tool/consul/agent.go> Add ChangeAllServiceNodes method in interface

--- tool/consul/agent.go
+++ tool/consul/agent.go
@@ -6,5 +6,7 @@ package consul
 import "github.com/micro/go-micro/v2/registry"
 
 type Agent interface {
+	// method to refresh all service node list
+	ChangeAllServiceNodes()  // add in v.1.1.6
 	GetNextServiceNode(service string) (*registry.Node, error)
 }

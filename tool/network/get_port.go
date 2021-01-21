ef784931dcf3430f3fa9b2d4507bf7de67cd141f
지농 <jinhong0719@naver.com>
2021-01-21 10:24:21 +0900

    [CREATE] <tool/network/get_port.go> Create file to declare func about getting port

---
+++ tool/network/get_port.go
@@ -0,0 +1,5 @@
+// Add package in v.1.1.6
+// network package in tool dir is used for utility about network like get available port, get my IP, etc ...
+// get_port.go is file to declare various function, not method, about getting port
+
+package network

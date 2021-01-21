3229e1e5929ec3e72af25af5ef0a7b01de76d161
지농 <jinhong0719@naver.com>
2021-01-21 10:26:32 +0900

    [ADD] <tool/network/get_port.go> Add function to get available port in range

--- tool/network/get_port.go
+++ tool/network/get_port.go
@@ -3,3 +3,22 @@
 // get_port.go is file to declare various function, not method, about getting port
 
 package network
+
+import (
+	"fmt"
+	"math/rand"
+	"net"
+)
+
+func GetRandomPortNotInUsedWithRange(min, max int) (port int) {
+	for {
+		port = rand.Intn(max - min) + min
+		conn, err := net.Listen("tcp", fmt.Sprintf(":%d", port))
+		if err != nil {
+			continue
+		}
+		_ = conn.Close()
+		break
+	}
+	return
+}

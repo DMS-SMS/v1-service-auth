f1f814d59ff09df0471b3bb9dc5730fb80422120
지농 <jinhong0719@naver.com>
2021-01-22 10:07:46 +0900

    [ADD] <tool/consul/agent/errors.go> Add ErrUnavailableService error

--- tool/consul/agent/errors.go
+++ tool/consul/agent/errors.go
@@ -6,5 +6,6 @@ package agent
 import "errors"
 
 var (
-	ErrAvailableNodeNotFound = errors.New("there is no currently available services")
+	ErrAvailableNodeNotFound = errors.New("there is no currently available service node")
+	ErrUnavailableService = errors.New("unavailable service, please put in agent.Services if you want to use")
 )

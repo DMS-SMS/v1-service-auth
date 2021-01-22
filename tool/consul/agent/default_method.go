c4369d304b24a5baa1b41b6b8d6af1a07783d803
지농 <jinhong0719@naver.com>
2021-01-22 09:57:48 +0900

    [MODIFY] <tool/consul/agent/default_method.go> Change logic of getting service node

--- tool/consul/agent/default_method.go
+++ tool/consul/agent/default_method.go
@@ -70,33 +70,13 @@ func (d *_default) changeServiceNodes(service consul.ServiceName) error {
 }
 
 // move from agent/default.go to agent/default_method.go
-func (d *_default) GetNextServiceNode(service string) (*registry.Node, error) {
-	checks, _, err := d.client.Health().Checks(service, &api.QueryOptions{Filter: StatusMustBePassing})
-	if err != nil {
-		return nil, errors.New(fmt.Sprintf("unable to query health checkes, err: %v", err))
-	}
-
-	if len(checks) == 0 {
+// migrate change logic to changeServiceNodes method in v.1.1.6
+func (d *_default) GetNextServiceNode(service consul.ServiceName) (*registry.Node, error) {
+	if len(d.nodes[service]) == 0 {
 		return nil, ErrAvailableNodeNotFound
 	}
 
-	var nodes []*registry.Node
-	for _, check := range checks {
-		as, _, err := d.client.Agent().Service(check.ServiceID, nil)
-		if err != nil {
-			return nil, errors.New(fmt.Sprintf("unable to query service, err: %v", err))
-		}
-		var md = map[string]string{"CheckID": check.CheckID}
-		node := &registry.Node{Id: as.ID, Address: fmt.Sprintf("%s:%d", as.Address, as.Port), Metadata: md}
-		nodes = append(nodes, node)
-	}
-
-	if !reflect.DeepEqual(d.nodes, nodes) {
-		d.nodes = nodes
-		d.next = d.Strategy([]*registry.Service{{Nodes: nodes}})
-	}
-
-	selectedNode, err := d.next()
+	selectedNode, err := d.next[service]()
 	if err != nil {
 		return nil, errors.New(fmt.Sprintf("unable to select node in selector, err: %v", err))
 	}

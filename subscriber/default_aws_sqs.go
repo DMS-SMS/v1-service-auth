4b4594f944b5cf6c6fee8d0b4412467581fd7f07
지농 <jinhong0719@naver.com>
2021-01-22 19:31:01 +0900

    [MOVE] <subscriber/listener_aws_sqs.go> Rename file from default_aws_sqs.go

--- subscriber/default_aws_sqs.go
+++
@@ -1,4 +0,0 @@
-// Add package in v.1.1.6
-// this file declare method that handling message came from aws sqs in _default struct
-
-package subscriber
---
+++ subscriber/listener_aws_sqs.go
@@ -0,0 +1,5 @@
+// Add package in v.1.1.6
+// listener is function that return closure used in subscribe
+// listener_aws_sqs.go is file that declare closure listening message from aws sqs
+
+package subscriber

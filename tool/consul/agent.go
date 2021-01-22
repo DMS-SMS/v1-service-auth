9f364413845f0493ae00d8ae83e09c6418387e52
지농 <jinhong0719@naver.com>
2021-01-22 15:21:49 +0900

    [ADD] <tool/consul/agent.go> Add method to return closure in interface

--- tool/consul/agent.go
+++ tool/consul/agent.go
@@ -3,7 +3,10 @@
 
 package consul
 
-import "github.com/micro/go-micro/v2/registry"
+import (
+	"github.com/micro/go-micro/v2/registry"
+	"github.com/micro/go-micro/v2/server"
+)
 
 type ServiceName string
 
@@ -13,4 +16,6 @@ type Agent interface {
 	// method to refresh specific service node list
 	ChangeServiceNodes(ServiceName) error // add in v.1.1.6
 	GetNextServiceNode(ServiceName) (*registry.Node, error)
+	ServiceNodeRegistry(server.Server) func() error   // add in v.1.1.6 (move from tool/closure/consul.go)
+	ServiceNodeDeregistry(server.Server) func() error // add in v.1.1.6 (move from tool/closure/consul.go)
 }

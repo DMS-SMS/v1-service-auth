76794a6ccfbdc8b7b0aee88e546780f327cec608
지농 <jinhong0719@naver.com>
2021-01-25 09:58:10 +0900

    [ADD] <subscriber/listener.go> Add sqsMsgHandler function signature type

--- subscriber/listener.go
+++ subscriber/listener.go
@@ -3,3 +3,10 @@
 // listener.go is file that declare closure listening message from aws sqs, rabbitMQ, etc ...
 
 package subscriber
+
+import (
+	"github.com/aws/aws-sdk-go/service/sqs"
+)
+
+// function signature type for sqs message handler
+type sqsMsgHandler func(*sqs.Message) error

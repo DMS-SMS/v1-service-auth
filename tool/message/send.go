237d6aba2fae0141ac0ac8be25cd06e47baeaad8
지농 <jinhong0719@naver.com>
2021-02-22 17:29:33 +0900

    [ADD] <tool/message/send.go> Add SendMassToReceivers function

--- tool/message/send.go
+++ tool/message/send.go
@@ -5,8 +5,13 @@
 package message
 
 import (
+	"encoding/json"
+	"errors"
+	"fmt"
 	"log"
+	"net/http"
 	"os"
+	"strconv"
 )
 
 var aligoAPIKey string
@@ -34,4 +39,59 @@ type SendMassToReceiversResponse struct {
 	SuccessCnt int    `json:"success_cnt"`
 	ErrorCnt   int    `json:"error_cnt"`
 	MsgType    string `json:"msg_type"`
+}
+
+func SendMassToReceivers(receivers, contents []string, _type, title string) (jsonResp SendMassToReceiversResponse, err error) {
+	if (len(receivers) != len(contents)) || (len(receivers) < 1 || len(contents) < 1) {
+		err = errors.New("receivers & contents must be same length bigger than 0")
+		return
+	}
+
+	if _type != "" && _type != "SMS" && _type != "LMS" && _type != "MMS" {
+		err = errors.New("type value must be blank or SMS or LMS or MMS")
+		return
+	}
+
+	if (_type == "SMS" || _type == "") && title != "" {
+		err = errors.New("cannot set title when type is black or SMS")
+		return
+	}
+
+	req, err := http.NewRequest("POST", "https://apis.aligo.in/send_mass/", nil)
+	if err != nil {
+		err = errors.New(fmt.Sprintf("some error occurs while creating request, err: %v", err))
+		return
+	}
+
+	q := req.URL.Query()
+	q.Add("key", aligoAPIKey)
+	q.Add("user_id", aligoAccountID)
+	q.Add("sender", aligoSender)
+	if _type != "" {
+		q.Add("msg_type", _type)
+	}
+	if title != "" {
+		q.Add("title", title)
+	}
+	for i, receiver := range receivers {
+		q.Add(fmt.Sprintf("rec_%d", i+1), receiver)
+		q.Add(fmt.Sprintf("msg_%d", i+1), contents[i])
+	}
+	q.Add("cnt", strconv.Itoa(len(receivers)))
+	req.URL.RawQuery = q.Encode()
+
+	resp, err := (&http.Client{}).Do(req)
+	if err != nil {
+		err = errors.New(fmt.Sprintf("some error occurs while sending request, err: %v", err))
+		return
+	}
+
+	jsonResp = SendMassToReceiversResponse{}
+	_ = json.NewDecoder(resp.Body).Decode(&jsonResp)
+	if resp.StatusCode != http.StatusOK || jsonResp.ResultCode != 0 {
+		err = errors.New(fmt.Sprintf("failed to send mass message, status: %d, json response: %v", resp.StatusCode, jsonResp))
+		return
+	}
+
+	return
 }

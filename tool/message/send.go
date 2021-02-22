e51774c11fc47d9910d284d550a813e58a42d35a
지농 <jinhong0719@naver.com>
2021-02-22 17:26:50 +0900

    [ADD] <tool/message/send.go> Add getting environment variable about ALIGO

--- tool/message/send.go
+++ tool/message/send.go
@@ -3,3 +3,26 @@
 // send.go is file that sending message
 
 package message
+
+import (
+	"log"
+	"os"
+)
+
+var aligoAPIKey string
+var aligoAccountID string
+var aligoSender string
+
+func init() {
+	if aligoAPIKey = os.Getenv("ALIGO_API_KEY"); aligoAPIKey == "" {
+		log.Fatal("please set ALIGO_API_KEY in environment variable")
+	}
+
+	if aligoAccountID = os.Getenv("ALIGO_ACCOUNT_ID"); aligoAccountID == "" {
+		log.Fatal("please set ALIGO_ACCOUNT_ID in environment variable")
+	}
+
+	if aligoSender = os.Getenv("ALIGO_SENDER"); aligoSender == "" {
+		log.Fatal("please set ALIGO_SENDER in environment variable")
+	}
+}

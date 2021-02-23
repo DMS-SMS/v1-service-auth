5d39dd7a0b5cb6bc1928231643bcf1c79bd0ad60
지농 <jinhong0719@naver.com>
2021-02-23 10:36:19 +0900

    [ADD] <db/access/default_update.go> Add setting  ParentStatus in ModifyStudentInform

--- db/access/default_update.go
+++ db/access/default_update.go
@@ -13,12 +13,13 @@ func (d *_default) ModifyStudentInform(uuid string, revisionInform *model.Studen
 		return
 	}
 
-	if revisionInform.Grade != emptyInt          { contextForUpdate[revisionInform.Grade.KeyName()] = revisionInform.Grade }
-	if revisionInform.Class != emptyInt          { contextForUpdate[revisionInform.Class.KeyName()] = revisionInform.Class }
-	if revisionInform.StudentNumber != emptyInt  { contextForUpdate[revisionInform.StudentNumber.KeyName()] = revisionInform.StudentNumber }
-	if revisionInform.Name != emptyString        { contextForUpdate[revisionInform.Name.KeyName()] = revisionInform.Name }
-	if revisionInform.PhoneNumber != emptyString { contextForUpdate[revisionInform.PhoneNumber.KeyName()] = revisionInform.PhoneNumber }
-	if revisionInform.ProfileURI != emptyString  { contextForUpdate[revisionInform.ProfileURI.KeyName()] = revisionInform.ProfileURI }
+	if revisionInform.Grade != emptyInt           { contextForUpdate[revisionInform.Grade.KeyName()] = revisionInform.Grade }
+	if revisionInform.Class != emptyInt           { contextForUpdate[revisionInform.Class.KeyName()] = revisionInform.Class }
+	if revisionInform.StudentNumber != emptyInt   { contextForUpdate[revisionInform.StudentNumber.KeyName()] = revisionInform.StudentNumber }
+	if revisionInform.Name != emptyString         { contextForUpdate[revisionInform.Name.KeyName()] = revisionInform.Name }
+	if revisionInform.PhoneNumber != emptyString  { contextForUpdate[revisionInform.PhoneNumber.KeyName()] = revisionInform.PhoneNumber }
+	if revisionInform.ProfileURI != emptyString   { contextForUpdate[revisionInform.ProfileURI.KeyName()] = revisionInform.ProfileURI }
+	if revisionInform.ParentStatus != emptyString { contextForUpdate[revisionInform.ParentStatus.KeyName()] = revisionInform.ParentStatus }
 
 	err = d.tx.Model(&model.StudentInform{}).Where("student_uuid = ?", uuid).Updates(contextForUpdate).Error
 	return
